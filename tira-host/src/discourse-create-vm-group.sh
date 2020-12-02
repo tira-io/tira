fa3b540e0af70345af2270cfc48ad024ecec3cd9
Bastian Grahm <bg40lecu@studserv.uni-leipzig.de>
2020-12-02 19:23:37 +0100

    add cURLs for creating group and inviting users

--- tira-host/src/discourse-create-vm-group.sh
+++ tira-host/src/discourse-create-vm-group.sh
@@ -43,6 +43,24 @@ export FLAGS_HELP
 FLAGS "$@" || exit 1  # Parse command line arguments.
 eval set -- "${FLAGS_ARGV}"
 
+create_group() {
+    group_name="vm-$vmname_or_user"
+    group_bio="Members of this group have access to the virtual machine $vmname_or_user on $host.<br><br>
+    The password for the virtual machine $vmname_or_user was sent to the participants in a separate mail.<br>The virtual machine can be accessed via SSH (host $host, port $port) or RDP (host $host, port XYZ) to install the software(s) that participate in the shared task.<br><br>You can SSH into the virtual machine with: sshpass -p $pw ssh $user@$host.medien.uni-weimar.de -p $port -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+
+    # Post form for group creation and store group id for invite
+    group_info=$(curl -X POST "https://disraptor.tira.io/admin/groups" -H "Api-Key: $api_key" -H "Accept: application/json" -H "Content-Type: multipart/form-data" -F "group[name]=$group_name" -F "group[messageable_level]=2" -F "group[member_visibility_level]=2" -F "group[bio_raw]=$group_bio")
+
+    group_id=$(group_info | jq '.basic_group.id')
+}
+
+invite_users() {
+    year=$(date +"%Y" -d 'next year')
+
+    # invite users to group created by create_group()
+    curl -X POST "https://disraptor.tira.io/invites/link" -H "Api-Key: $api_key" -H "Accept: application/json" -H "Content-Type: multipart/form-data" -F "group_ids[]=$group_id" -F "max_redemptions_allowed=20" -F "expires_at=$year-12-31"
+}
+
 #
 #    Create the discourse group associated to a vm.
 #
@@ -58,7 +76,7 @@ main() {
     vm_info=$(get_vm_info_from_tira "$vmname_or_user")
 
     if [ "$vm_info" = "" ]; then
-        logError "VM-Name/user $vmname_or_user is not registred. Use tira vm-list to get a list of all vms."
+        logError "VM-Name/user $vmname_or_user is not registered. Use tira vm-list to get a list of all vms."
         exit 1
     fi
     logDebug "Extracted vm info: \n$vm_info"
