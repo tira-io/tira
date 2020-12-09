0b7609e37ee5d4c4d71ab6a5128d83cf1558f85b
Nikolay Kolyada <nikolay.kolyada@uni-weimar.de>
2020-12-09 14:32:39 +0100

    Working on grpc client

--- tira-application/src/tira/urls.py
+++ tira-application/src/tira/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('tasks', views.task_list, name='task'),
     path('task/<str:task_id>', views.task_detail, name='task-detail'),
     path('task/<str:task_id>/user/<str:user_id>', views.software_detail, name='software-detail'),
+    path('task/<str:task_id>/user/<str:user_id>/<str:action>', views.software_detail, name='software-detail'),
     path('dataset', views.dataset_list, name='dataset'),
     path('dataset/<str:dataset_id>', views.dataset_detail, name='dataset-detail'),
     path('software/<str:user_id>', views.software_user, name='software-user'),
