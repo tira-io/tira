4118088b0682ed5fafe82e0404174165a6c9ecb4
Matti Wiegmann <matti.wiegmann@uni-weimar.de>
2020-12-15 17:10:11 +0100

    update model interface to more clearly separate model and views

--- tira-application/src/tira/tira_model.py
+++ tira-application/src/tira/tira_model.py
@@ -21,8 +21,16 @@ class FileDatabase(object):
 
     def __init__(self):
         logger.info("Start loading dataset")
-        self.organizers = self.vms = self.tasks = self.datasets = self.software = None
-        self.software_by_task = self.software_by_vm = self.software_count_by_dataset = None
+        self.organizers = None  # dict of host objects host_id: modelpb.Host
+        self.vms = None  # dict of vm_id: modelpb.User
+        self.tasks = None  # dict of task_id: modelpb.Tasks.Task
+        self.datasets = None  # dict of dataset_id: modelpb.Dataset
+        self.software = None  # dict of task_id$vm_id: modelpb.Software
+        self.default_tasks = None  # dataset_id: task_id
+        self.task_organizers = None  # dataset_id: modelpb.Hosts.Host.name
+        self.software_by_task = None  # task_id: [modelpb.Software]
+        self.software_by_vm = None  # vm_id: [modelpb.Software]
+        self.software_count_by_dataset = None  # dataset_id: int()
 
         self._parse_organizer_list()
         self._parse_vm_list()
@@ -139,75 +147,102 @@ class FileDatabase(object):
     def _load_review(self, dataset_id, vm_id, run_id):
         review_path = self.RUNS_DIR_PATH / dataset_id / vm_id / run_id / "run-review.bin"
         if not review_path.exists():
-            return {"reviewer": None}
+            review = modelpb.RunReview()
+            review.reviewerId = ""
+            return review
         review = modelpb.RunReview()
         review.ParseFromString(open(review_path, "rb").read())
-        return {
-            "reviewer": review.reviewerId, "noErrors": review.noErrors, "missingOutput": review.missingOutput,
-            "extraneousOutput": review.extraneousOutput, "invalidOutput": review.invalidOutput,
-            "hasErrorOutput": review.hasErrorOutput, "otherErrors": review.otherErrors,
-            "comment": review.comment, "hasErrors": review.hasErrors, "hasWarnings": review.hasWarnings,
-            "hasNoErrors": review.hasNoErrors, "published": review.published, "blinded": review.blinded
-        }
+        return review
 
-    # TODO store review objects in _loads
-    def _load_user_runs(self, dataset_id, user_id):
-        """
-        :return runs: {run_id: {}}
+    def _load_user_reviews(self, dataset_id, vm_id):
+        reviews = {}
+        for run_id_dir in (self.RUNS_DIR_PATH / dataset_id / vm_id).glob("*"):
+            if not (run_id_dir / "run.bin").exists():
+                review = modelpb.RunReview()
+                review.runId = run_id_dir.stem
+                review.reviewerId = 'tira'
+                review.reviewDate = ""
+                review.comment = "Internal Error: No run definition recorded. Please contact the support."
+                review.hasErrors = True
+                review.hasWarnings = False
+                review.hasNoErrors = False
+                review.blinded = False
+
+                reviews[run_id_dir.stem] = review
+                continue
+
+            reviews[run_id_dir.stem] = self._load_review(dataset_id, vm_id, run_id_dir.stem)
+
+        return reviews
+
+    def _get_review(self, review):
+        return {"reviewer": review.reviewerId, "noErrors": review.noErrors, "missingOutput": review.missingOutput,
+                "extraneousOutput": review.extraneousOutput, "invalidOutput": review.invalidOutput,
+                "hasErrorOutput": review.hasErrorOutput, "otherErrors": review.otherErrors,
+                "comment": review.comment, "hasErrors": review.hasErrors, "hasWarnings": review.hasWarnings,
+                "hasNoErrors": review.hasNoErrors, "published": review.published, "blinded": review.blinded
+                }
+
+    def _load_vm_runs(self, dataset_id, vm_id, include_evaluations):
+        """ load all run's data.
+        @param include_evaluations: If True, also load evaluator runs (where an evaluation.bin exists)
         """
         runs = {}
-        for run_id_dir in (self.RUNS_DIR_PATH / dataset_id / user_id).glob("*"):
+        for run_id_dir in (self.RUNS_DIR_PATH / dataset_id / vm_id).glob("*"):
             if not (run_id_dir / "run.bin").exists():
-                runs[run_id_dir.stem] = {"software": "", "input_run_id": "", "run_id": run_id_dir.stem,
-                                         "dataset": dataset_id, "size": 0, "lines": 0, "files": 0, "dirs": 0,
-                                         "review": {"reviewer": "tira", "otherErrors": True, "hasErrors": True,
-                                                    "blinded": False, "published": False,
-                                                    "comment": "Software execution failed with NRFC-01. Please contact support."}}
+                run = modelpb.Run()
+                run.softwareId = ""
+                run.runId = run_id_dir.stem
+                run.inputDataset = dataset_id
+                runs[run_id_dir.stem] = run
                 continue
+
             run = modelpb.Run()
             run.ParseFromString(open(run_id_dir / "run.bin", "rb").read())
-            if not run.deleted:
-                runs[run.runId] = {"software": run.softwareId,
-                                   "run_id": run.runId, "input_run_id": run.inputRun,
-                                   "dataset": dataset_id, "size": "", "lines": "", "files": "", "dirs": "",
-                                   "downloadable": run.downloadable,
-                                   "review": self._load_review(dataset_id, user_id, run_id_dir.stem)}
+            if run.deleted:
+                continue
+            if include_evaluations is False and (run_id_dir / "output" / "evaluation.bin").exists():
+                continue
+            runs[run.runId] = self._get_run(run)
         return runs
 
-    # TODO store review objects in _loads
-    def _load_user_evaluations(self, dataset_id, user_id, runs, only_published=True):
+    def _get_run(self, run):
+        return {"software": run.softwareId,
+                "run_id": run.runId, "input_run_id": run.inputRun,
+                "dataset": run.inputDataset, "downloadable": run.downloadable}
+
+    def _load_vm_evaluations(self, dataset_id, vm_id, only_published):
         """ load all evaluations for a user on a given dataset
 
         :param dataset_id: id/name of the dataset
-        :param user_id: id/name of the user
+        :param vm_id: id/name of the user
         :param runs: a run dict as loaded by _load_user_runs: {run_id: {software, run_id, input_run_id, size, lines, files, dirs, downloadable, review}}
         :return 1: keys of the measures
-        :return 2: [{run_id, user_id, software, input_run_id, measures: {}, runtime}]
+        :return 2: [{run_id, vm_id, software, input_run_id, measures: {}, runtime}]
         """
-        evaluations = []
-        measure_keys = set()
-        for run_id_dir in (self.RUNS_DIR_PATH / dataset_id / user_id).glob("*"):
-            if not runs[run_id_dir.stem]["review"].get("reviewer", False):
-                continue
-            if runs[run_id_dir.stem]["review"].get("published", False) is False and only_published is True:
-                continue
+        evaluations = {}
+        # measure_keys = set()
+        for run_id_dir in (self.RUNS_DIR_PATH / dataset_id / vm_id).glob("*"):
             if not (run_id_dir / "output/evaluation.bin").exists():
+                continue
+            # if not runs[run_id_dir.stem]["review"].get("reviewer", False):
+            #     continue
+            if only_published is True and self._load_review(dataset_id, vm_id, run_id_dir.stem).published is False:
                 continue
 
             evaluation = modelpb.Evaluation()
             evaluation.ParseFromString(open(run_id_dir / "output/evaluation.bin", "rb").read())
 
-            if not measure_keys:
-                measure_keys.update({measure.key for measure in evaluation.measure})
+            # if not measure_keys:
+            #     measure_keys.update({measure.key for measure in evaluation.measure})
+            #
+            # measures = {measure.key: measure.value for measure in evaluation.measure}
+            evaluations[run_id_dir.stem] = evaluation
 
-            measures = {measure.key: measure.value for measure in evaluation.measure}
-            evaluations.append({"run_id": run_id_dir.stem, "user_id": user_id,
-                                "published": runs[run_id_dir.stem]["review"].get("published", False),
-                                "software": runs[run_id_dir.stem]["software"],
-                                "input_run_id": runs[run_id_dir.stem]["input_run_id"],
-                                "measures": measures, "runtime": ""})
+        return evaluations
 
-        return list(measure_keys), evaluations
+    def _get_evaluation(self, evaluation):
+        return {measure.key: measure.value for measure in evaluation.measure}
 
     # get methods are the public interface.
     def get_tasks(self) -> list:
@@ -247,6 +282,10 @@ class FileDatabase(object):
             "software_count": self.software_count_by_dataset.get(dataset.datasetId, 0)
         }
 
+    def get_datasets(self) -> dict:
+        """ Get a dict of dataset_id: dataset_json_descriptor """
+        return {dataset_id: self.get_dataset(dataset_id) for dataset_id in self.datasets.keys()}
+
     def get_datasets_by_task(self, task_id: str, include_deprecated=False) -> list:
         """ return the list of datasets associated with this task_id
         @param task_id: id string of the task the dataset belongs to
@@ -258,55 +297,72 @@ class FileDatabase(object):
                 if task_id == self.default_tasks.get(dataset.datasetId, "") and
                 not (dataset.isDeprecated and not include_deprecated)]
 
-    # TODO change accordingly with _load_runs
-    def get_dataset_runs(self, dataset_id, only_public_results=True) -> tuple:
-        """ return for all users on a given dataset_id: runs, evaluations, user_stats
-        Its equivalent to using the individual getters, but much faster
+    # # TODO change accordingly with _load_runs
+    # # TODO should actually give us a list of all runs done on this dataset (without grouping)
+    # def get_dataset_runs(self, dataset_id, only_public_results=True) -> tuple:
+    #     """ return all runs for all users on a given dataset_id """
+    #     status = {}
+    #     runs = {}
+    #     evaluations = {}
+    #     ev_keys = set()
+    #     for user_run_dir in (self.RUNS_DIR_PATH / dataset_id).glob("*"):
+    #         # all of these are dicts
+    #         user_runs = self._load_vm_runs(dataset_id, user_run_dir.stem)
+    #         user_evaluations = self._load_vm_evaluations(dataset_id, user_run_dir.stem,
+    #                                                      only_published=only_public_results)
+    #         user_reviews = self._load_user_reviews(dataset_id, user_run_dir.stem)
+    #
+    #         keys = set()
+    #         for e in ev.values():
+    #             keys.update(e.keys())
+    #         keys = list(keys)
+    #
+    #         # runs[user_run_dir.stem] = list(r.values())
+    #
+    #         # update the measures evaluations to a list, following the order of the keys
+    #         evaluations[user_run_dir.stem] = ev
+    #         for eval in evaluations[user_run_dir.stem]:
+    #             m = eval["measures"]
+    #             eval["measures"] = [m[k] for k in keys]
+    #
+    #         unreviewed_count = sum([1 for x in runs[user_run_dir.stem] if not x["review"].get("reviewer", None)])
+    #         status[user_run_dir.stem] = {"user_id": user_run_dir.stem,
+    #                                      "signed_in": "", "softwares": "", "deleted": "", "now_running": "",
+    #                                      "runs": len(runs[user_run_dir.stem]),
+    #                                      "reviewed": "", "unreviewed": unreviewed_count}  # TODO dummy
+    #
+    #     return ev_keys, status, runs, evaluations
 
-        @return ev_keys: keys of the measures used in this dataset
-        @return user: dict of users and runs
-        @return status: {user_id: {signed_in, softwares, deleted, now_running, runs, reviewed, unreviewed}}
-        runs: {user_id: [{software, size, lines, files, dirs, review: {}}],
-        evaluations: {user_id: [{software, run_id, input_run_id, measures: {}, runtime}]}
-        """
-        status = {}
-        runs = {}
-        evaluations = {}
-        ev_keys = set()
-        for user_run_dir in (self.RUNS_DIR_PATH / dataset_id).glob("*"):
-            r = self._load_user_runs(dataset_id, user_run_dir.stem)
-            keys, ev = self._load_user_evaluations(dataset_id, user_run_dir.stem, r, only_published=only_public_results)
-            runs[user_run_dir.stem] = list(r.values())
+    def get_vms_by_dataset(self, dataset_id):
+        """ return a list of vm_id's that have runs on this dataset """
+        return [user_run_dir.stem
+                for user_run_dir in (self.RUNS_DIR_PATH / dataset_id).glob("*")]
 
-            # as keys for the measures, we use the first non-empty set of keys we encounter
-            if keys and not ev_keys:
-                ev_keys = keys
+    def get_vm_runs_by_dataset(self, dataset_id, vm_id, include_evaluations=True):
+        user_runs = self._load_vm_runs(dataset_id, vm_id, include_evaluations)
+        return list(user_runs.values())
 
-            # update the measures evaluations to a list, following the order of the keys
-            evaluations[user_run_dir.stem] = ev
-            for eval in evaluations[user_run_dir.stem]:
-                m = eval["measures"]
-                eval["measures"] = [m[k] for k in keys]
-
-            unreviewed_count = sum([1 for x in runs[user_run_dir.stem] if not x["review"].get("reviewer", None)])
-            status[user_run_dir.stem] = {"user_id": user_run_dir.stem,
-                                         "signed_in": "", "softwares": "", "deleted": "", "now_running": "",
-                                         "runs": len(runs[user_run_dir.stem]),
-                                         "reviewed": "", "unreviewed": unreviewed_count}  # TODO dummy
-
-        return ev_keys, status, runs, evaluations
-
-    # TODO change accordingly with _load_runs
-    def get_user_runs(self, task_id, vm_id):
+    def get_vm_runs_by_task(self, task_id, vm_id, include_evaluations=True):
         """ returns a list of all the runs of a user over all datasets in json (as returned by _load_user_runs) """
         relevant_datasets = {software["dataset"] for software in self.get_software(task_id, vm_id)}
-
         runs = []
         for dataset_id in relevant_datasets:
-            user_runs = self._load_user_runs(dataset_id, vm_id)
-            runs.extend(list(user_runs.values()))
-
+            runs.extend(self.get_vm_runs_by_dataset(dataset_id, vm_id, include_evaluations))
         return runs
+
+    def get_vm_evaluations_by_dataset(self, dataset_id, vm_id, only_public_results=True):
+        """ Return a dict of run_id: evaluation_results for the given vm on the given dataset
+        @param only_public_results: only return the measures for published datasets.
+        """
+        return {run_id: self._get_evaluation(ev)
+                for run_id, ev in self._load_vm_evaluations(dataset_id, vm_id, only_published=only_public_results).items()}
+
+    def get_vm_run_reviews(self):
+        pass
+
+    def get_vm_reviews_by_dataset(self, dataset_id, vm_id):
+        return {run_id: self._get_review(review)
+                for run_id, review in self._load_user_reviews(dataset_id, vm_id).items()}
 
     def get_software(self, task_id, vm_id):
         """ Returns the software of a vm on a task in json """
