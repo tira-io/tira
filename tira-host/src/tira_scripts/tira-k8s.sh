80e9254ddab466d1ce9f06e62863a7389b54584a
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2021-01-30 07:04:23 +0100

    Add early exit for k8s commands to improve #32

--- tira-host/src/tira_scripts/tira-k8s.sh
+++ tira-host/src/tira_scripts/tira-k8s.sh
@@ -33,8 +33,6 @@ Examples:
     exit 1
 }
 
-
-
 POD=$(kubectl -n default get all|grep tira-bg-web-client|grep 'Running'|grep -v 'tira-web-client-2-'|head -1|awk '{print }')
 
 if [ -z "${POD}" ]
@@ -43,5 +41,6 @@ then
 	exit 1
 fi
 
-kubectl -n default exec -ti ${POD} -- bash -c "cd /usr/local/share/tira/src && ${@}"
+INTERNAL_CMD="${@}"
+kubectl -n default exec -ti ${POD} -- bash -c "cd /usr/local/share/tira/src && ${INTERNAL_CMD}"
 
