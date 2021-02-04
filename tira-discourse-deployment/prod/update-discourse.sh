e1b494dea7def82988214f5ee3455f57873b3132
Bastian Grahm <bg40lecu@studserv.uni-leipzig.de>
2021-02-04 16:54:14 +0100

    [FIX] change image in deployment

--- tira-discourse-deployment/prod/update-discourse.sh
+++ tira-discourse-deployment/prod/update-discourse.sh
@@ -88,6 +88,7 @@ cd $BASE_DIRECTORY
 # cp prod/setup /mnt/ceph/storage/data-in-production/disraptor/boot/setup
 
 # redeploy
+sed "s/image: .*$DOCKERHUB_IMAGENAME.*/image: docker.io/$DOCKERHUB_USER/$DOCKERHUB_IMAGENAME:$DOCKERHUB_TAG/g"
 #prod/k8s-undeploy-discourse-prod.sh
 #prod/k8s-deploy-discourse-prod.sh
 
