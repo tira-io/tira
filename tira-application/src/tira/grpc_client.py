0d6ea222c36fbb299704b100bbef2b1397ceec0b
Nikolay Kolyada <nikolay.kolyada@uni-weimar.de>
2021-02-18 14:43:54 +0100

    grpc-client: remove command queue. Changes to discourse-create-vm-group.sh.

--- tira-application/src/tira/grpc_client.py
+++ tira-application/src/tira/grpc_client.py
@@ -25,40 +25,40 @@ import threading
 
 from .proto import tira_host_pb2, tira_host_pb2_grpc
 
-
 grpc_port = settings.GRPC_PORT
-tasks = {}
+# tasks = {}
 
 
 def task_status_url(task_id):
-    return '/command_status/'+task_id
+    return '/command_status/' + task_id
 
 
-def async_api(wrapped_function):
-    @wraps(wrapped_function)
-    def new_function(*args, **kwargs):
-        def task_call():
-            try:
-                tasks[task_id]['return_value'] = wrapped_function(*args, **kwargs)
-            except Exception as e:
-                tasks[task_id]['return_value'] = HttpResponse(status=500)
-            finally:
-                # We record the time of the response, to help in garbage
-                # collecting old tasks
-                tasks[task_id]['completion_timestamp'] = datetime.timestamp(datetime.utcnow())
-
-        # Assign an id to the asynchronous task
-        task_id = uuid.uuid4().hex
-
-        # Record the task, and then launch it
-        tasks[task_id] = {'task_thread': threading.Thread(target=task_call, args=())}
-        tasks[task_id]['task_thread'].start()
-
-        # Return a 202 response, with a link that the client can use to obtain task status
-        print(task_status_url(task_id))
-        # return 'accepted', 202, {'Location': command_status_url('/command_status/'+task_id, task_id=task_id)}
-        return (202, task_status_url(task_id))
-    return new_function
+# def async_api(wrapped_function):
+#     @wraps(wrapped_function)
+#     def new_function(*args, **kwargs):
+#         def task_call():
+#             try:
+#                 tasks[task_id]['return_value'] = wrapped_function(*args, **kwargs)
+#             except Exception as e:
+#                 tasks[task_id]['return_value'] = HttpResponse(status=500)
+#             finally:
+#                 # We record the time of the response, to help in garbage
+#                 # collecting old tasks
+#                 tasks[task_id]['completion_timestamp'] = datetime.timestamp(datetime.utcnow())
+#
+#         # Assign an id to the asynchronous task
+#         task_id = uuid.uuid4().hex
+#
+#         # Record the task, and then launch it
+#         tasks[task_id] = {'task_thread': threading.Thread(target=task_call, args=())}
+#         tasks[task_id]['task_thread'].start()
+#
+#         # Return a 202 response, with a link that the client can use to obtain task status
+#         print(task_status_url(task_id))
+#         # return 'accepted', 202, {'Location': command_status_url('/command_status/'+task_id, task_id=task_id)}
+#         return (202, task_status_url(task_id))
+#
+#     return new_function
 
 
 class GrpcClient:
@@ -70,7 +70,6 @@ class GrpcClient:
     def __del__(self):
         self.channel.close()
 
-    @async_api
     def vm_start(self, vm_name):
         response = self.stub.vm_start(tira_host_pb2.RequestVmCommands(vmName=vm_name))
         print("Client received: " + response.output)
@@ -82,50 +81,46 @@ class GrpcClient:
         #
         # return HttpResponse(status=202)
 
-    @async_api
+
     def vm_stop(self, vm_name):
         response = self.stub.vm_stop(tira_host_pb2.RequestVmCommands(vmName=vm_name))
         print("Client received: " + response.output)
         return response.output
 
-    @async_api
     def vm_shutdown(self, vm_name):
         response = self.stub.vm_shutdown(tira_host_pb2.RequestVmCommands(vmName=vm_name))
         print("Client received: " + response.output)
         return response.output
 
-    @async_api
     def vm_info(self, vm_name):
         response_vm_info = self.stub.vm_info(tira_host_pb2.RequestVmCommands(vmName=vm_name))
         print("Client received: " + str(response_vm_info))
         return 202, response_vm_info
 
-    @async_api
     def vm_list(self, vm_name):
         response = self.stub.vm_list(Empty)
         print("Client received: " + response.output)
         return response.output
 
-    @async_api
-    def run_execute(self, vm_name):
-        response = self.stub.run_execute(tira_host_pb2.RequestRunExecuteEval(submissionFile="",
-                                                                             inputDatasetName="",
-                                                                             inputRunPath="",
-                                                                             outputDirName="",
-                                                                             sandboxed="",
-                                                                             optionalParameters=""
+    def run_execute(self, submissionFile, inputDatasetName, inputRunPath, outputDirName, sandboxed, optionalParameters):
+        response = self.stub.run_execute(tira_host_pb2.RequestRunExecuteEval(submissionFile=submissionFile,
+                                                                             inputDatasetName=inputDatasetName,
+                                                                             inputRunPath=inputRunPath,
+                                                                             outputDirName=outputDirName,
+                                                                             sandboxed=sandboxed,
+                                                                             optionalParameters=optionalParameters
                                                                              ))
         print("Client received: " + response.output)
         return response.output
 
-    @async_api
-    def run_eval(self, vm_name):
-        response = self.stub.run_eval(tira_host_pb2.RequestRunExecuteEval(submissionFile="",
-                                                                          inputDatasetName="",
-                                                                          inputRunPath="",
-                                                                          outputDirName="",
-                                                                          sandboxed="",
-                                                                          optionalParameters=""
+    def run_eval(self, vm_name, submissionFile, inputDatasetName, inputRunPath, outputDirName, sandboxed,
+                 optionalParameters):
+        response = self.stub.run_eval(tira_host_pb2.RequestRunExecuteEval(submissionFile=submissionFile,
+                                                                          inputDatasetName=inputDatasetName,
+                                                                          inputRunPath=inputRunPath,
+                                                                          outputDirName=outputDirName,
+                                                                          sandboxed=sandboxed,
+                                                                          optionalParameters=optionalParameters
                                                                           ))
         print("Client received: " + response.output)
         return response.output
@@ -139,7 +134,7 @@ class GrpcClient:
             status code, it means that task hasn't finished yet. Else, the response
             from the task is returned.
         """
-        task = tasks.get(task_id)
+        # task = tasks.get(task_id)
         if task is None:
             return 404
         if 'return_value' not in task:
