e0f60e868fffa70148f5c0bece37cfdfb26ca230
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2021-03-11 19:47:22 +0100

    try to solve #53

--- tira-host/src/tira_scripts/discourse-create-vm-group.py
+++ tira-host/src/tira_scripts/discourse-create-vm-group.py
@@ -13,7 +13,7 @@ def discourse_invite_link(user):
     cmd = [SCRIPT_DISCOURSE, user]
     try:
         ret = subprocess.check_output(cmd)
-        return ret.split('\n')[-1]
+        return ret.split('\n')[-2]
     except:
         return 'Error during the creation of the discourse invite-link. Please run "' + (' '.join(cmd)) + '".'
 
