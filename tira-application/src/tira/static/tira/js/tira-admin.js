e5f998308f774ec7e0b8fad1ecebe324adc3c1ba
MattiWe <mattiwiegmann@gmail.com>
2021-03-20 00:34:00 +0100

    asdasda

--- tira-application/src/tira/static/tira/js/tira-admin.js
+++ tira-application/src/tira/static/tira/js/tira-admin.js
@@ -12,6 +12,25 @@ function reloadData() {
     })
 }
 
+function string_to_slug (str) {
+    str = str.replace(/^\s+|\s+$/g, ''); // trim
+    str = str.toLowerCase();
+
+    // remove accents, swap ñ for n, etc
+    var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+    var to   = "aaaaeeeeiiiioooouuuunc------";
+    for (var i=0, l=from.length ; i<l ; i++) {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
+
+    str = str.replace(/\./g, '-')
+        .replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+        .replace(/\s+/g, '-') // collapse whitespace and replace by -
+        .replace(/-+/g, '-'); // collapse dashes
+
+    return str;
+}
+
 function submitCreateVmForm(){
     $('#create-vm-form-icon').html(' <div uk-spinner="ratio: 0.5"></div>')
     $.ajax({
@@ -37,6 +56,10 @@ function addTiraAdminHandlers() {
     $('#create-vm-from').submit(function(e) {
         e.preventDefault();
         submitCreateVmForm()
+    })
+    $('#id_dataset_name').keypress(function() {
+        let name = this.val()
+        $('#id_dataset_id_prefix').val(string_to_slug(name))
     })
 }
 
