4bed50ee68642c8e69f3dab545fe6699b2b82b7a
Bastian Grahm <bg40lecu@studserv.uni-leipzig.de>
2021-03-30 15:37:10 +0200

    [FIX] update discourse deployment files

--- tira-discourse-deployment/prod/k8s-deploy-discourse-prod.sh
+++ tira-discourse-deployment/prod/k8s-deploy-discourse-prod.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if kubectl get secret | grep -q discourse-prod-secret
+if kubectl get secret -n webisservices | grep -q discourse-prod-secret
 then
     echo "Applying Discourse production environment"
     ./k8s-deploy-discourse-prod-volume.sh
-    kubectl apply -f discourse-prod.yml
+    kubectl apply -f discourse-prod.yml -n webisservices
 else
     echo "Secret 'discourse-prod-secret' does not exist. Create first!"
 fi
