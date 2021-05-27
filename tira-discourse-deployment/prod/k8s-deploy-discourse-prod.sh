d122fa6aff2e95d66e35d850a0d87826eee013bf
Bastian Grahm <bg40lecu@studserv.uni-leipzig.de>
2021-05-27 11:38:47 +0200

    update image version and namespace

--- tira-discourse-deployment/prod/k8s-deploy-discourse-prod.sh
+++ tira-discourse-deployment/prod/k8s-deploy-discourse-prod.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if kubectl get secret -n webisservices | grep -q discourse-prod-secret
+if kubectl get secret -n tira | grep -q discourse-prod-secret
 then
     echo "Applying Discourse production environment"
     ./k8s-deploy-discourse-prod-volume.sh
-    kubectl apply -f discourse-prod.yml -n webisservices
+    kubectl apply -f discourse-prod.yml -n tira
 else
     echo "Secret 'discourse-prod-secret' does not exist. Create first!"
 fi
