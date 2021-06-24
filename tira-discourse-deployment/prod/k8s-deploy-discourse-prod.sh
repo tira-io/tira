09cf8edf0cd97087b61dd553ec2b3ddadbf949d6
Bastian Grahm <bg40lecu@studserv.uni-leipzig.de>
2021-06-24 18:17:55 +0200

    [CHG] Namespace name

--- tira-discourse-deployment/prod/k8s-deploy-discourse-prod.sh
+++ tira-discourse-deployment/prod/k8s-deploy-discourse-prod.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if kubectl get secret -n tira | grep -q discourse-prod-secret
+if kubectl get secret -n services-tira | grep -q discourse-prod-secret
 then
     echo "Applying Discourse production environment"
     ./k8s-deploy-discourse-prod-volume.sh
-    kubectl apply -f discourse-prod.yml -n tira
+    kubectl apply -f discourse-prod.yml -n services-tira
 else
     echo "Secret 'discourse-prod-secret' does not exist. Create first!"
 fi
--- tira-discourse-deployment/prod/k8s-undeploy-discourse-prod.sh
+++ tira-discourse-deployment/prod/k8s-undeploy-discourse-prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Deleting discourse-prod.yml"
-kubectl delete -f discourse-prod.yml -n tira || echo "discourse-prod.yml is already deleted"
+kubectl delete -f discourse-prod.yml -n services-tira || echo "discourse-prod.yml is already deleted"
