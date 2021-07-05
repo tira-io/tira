cc280d3f8f4660ec1054b978a5392023a7b88d06
Bastian Grahm <bg40lecu@studserv.uni-leipzig.de>
2021-07-05 18:10:01 +0200

    [ADD] reload vm state div

--- tira-application/src/tira/static/tira/js/software.js
+++ tira-application/src/tira/static/tira/js/software.js
@@ -8,7 +8,7 @@ function startVM(uid, vmid) {
             if(data.status === 'Accepted'){
                 $('#vm_power_button button:first-child').text('Shut Down');
                 $('#vm_power_button button:first-child').addClass('uk-button-danger');
-                $('#vm_power_button button:first-child').off('click').click(function() {stopVM(uid, vmid)})
+                $('#vm_power_button button:first-child').off('click').click(function() {shutdownVM(uid, vmid)})
                 $('#stop_vm_button button:first-child').addClass('uk-button-primary');
                 $('#stop_vm_button button:first-child').prop('disabled', false);
                 $('#abort_run_button button:first-child').addClass('uk-button-primary');
@@ -19,7 +19,7 @@ function startVM(uid, vmid) {
     })
 }
 
-function stopVM(uid, vmid) {
+function shutdownVM(uid, vmid) {
     $.ajax({
         type: 'GET',
         url: `/user/${uid}/vm/${vmid}/vm_stop`,
@@ -39,10 +39,31 @@ function stopVM(uid, vmid) {
     })
 }
 
+function stopVM() {
+
+}
+
+function abortRun(){
+
+}
+
+// Every 10s reload the content of the #vm_state div.
+function reloadVmState(url){
+    setTimeout(function() {
+        if (location.href == url){
+            console.log("Reloading VM State");
+            $('#vm_state').load(location.href + ' #vm_state>*', '');
+            reloadVmState(url);
+        }
+    }, 10000);
+}
+
 function addSoftwareEvents(uid, vmid) {
     if($('#vm_power_button button:first-child').text() == 'Power On') {
         $('#vm_power_button button:first-child').click(function() {startVM(uid, vmid)});
     } else {
-        $('#vm_power_button button:first-child').click(function() {stopVM(uid, vmid)});
+        $('#vm_power_button button:first-child').click(function() {shutdownVM(uid, vmid)});
     }
+
+    reloadVmState(location.href);
 }
