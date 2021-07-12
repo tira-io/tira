98ce1f195da631d0e85d60f3c79847d8fd92163b
Matti Wiegmann <matti.wiegmann@uni-weimar.de>
2021-07-12 19:00:26 +0200

    test grpc host now keeps a state

--- tira-application/test/grpc/test_grpc_host_server.py
+++ tira-application/test/grpc/test_grpc_host_server.py
@@ -3,9 +3,12 @@
 from concurrent import futures
 import grpc
 import sys
+from time import sleep
 
 sys.path.append('../../src/tira')
 from proto import tira_host_pb2, tira_host_pb2_grpc
+
+STATE = {"status": 'stopped'}
 
 
 class TiraHostService(tira_host_pb2_grpc.TiraHostService):
@@ -17,19 +20,22 @@ class TiraHostService(tira_host_pb2_grpc.TiraHostService):
     def vm_backup(self, request, context):
         print(f"received vm-backup for {request.vmName}")
         response = tira_host_pb2.Response()
-        response.commandId = tira_host_pb2.Response.Status.SUCCESS
+        response.status = tira_host_pb2.Response.Status.SUCCESS
+        response.commandId = "12345"
         return response
 
     def vm_create(self, request, context):
         print(f"received vm-create for {request.ovaFile} - {request.userName} - {request.bulkCommandId}")
         response = tira_host_pb2.Response()
-        response.commandId = tira_host_pb2.Response.Status.SUCCESS
+        response.status = tira_host_pb2.Response.Status.SUCCESS
+        response.commandId = "12345"
         return response
 
     def vm_delete(self, request, context):
         print(f"received vm-delete for {request.vmName}")
         response = tira_host_pb2.Response()
-        response.commandId = tira_host_pb2.Response.Status.SUCCESS
+        response.status = tira_host_pb2.Response.Status.SUCCESS
+        response.commandId = "12345"
         return response
 
     def vm_info(self, request, context):
@@ -38,66 +44,101 @@ class TiraHostService(tira_host_pb2_grpc.TiraHostService):
         response.guestOs = 'test: ubuntu'
         response.memorySize = 'test: 0'
         response.numberOfCpus = 'test: 0'
-        response.state = 'running'
         response.sshPort = '0000'
         response.rdpPort = '0000'
         response.host = 'localhost'
-        response.sshPortStatus = False
-        response.rdpPortStatus = True
+        if STATE.get('status') == 'stopped':
+            response.state = 'stopped'
+            response.sshPortStatus = False
+            response.rdpPortStatus = False
+        elif STATE.get('status') == 'running':
+            response.state = 'running'
+            response.sshPortStatus = True
+            response.rdpPortStatus = True
+        elif STATE.get('status') == 'sandboxed':
+            response.state = 'sandboxed'
+            response.sshPortStatus = False
+            response.rdpPortStatus = False
+        elif STATE.get('status') == 'sandboxing':
+            response.state = 'sandboxing'
+            response.sshPortStatus = False
+            response.rdpPortStatus = False
+
         return response
 
     def vm_list(self, context):
         print(f"received vm-list")
         response = tira_host_pb2.Response()
-        response.commandId = tira_host_pb2.Response.Status.SUCCESS
+        response.status = tira_host_pb2.Response.Status.SUCCESS
+        response.commandId = "12345"
         return response
 
     def vm_sandbox(self, request, context):
         print(f"received vm-sandbox for {request.vmName}")
         response = tira_host_pb2.Response()
-        response.commandId = tira_host_pb2.Response.Status.SUCCESS
+        response.status = tira_host_pb2.Response.Status.SUCCESS
+        response.commandId = "12345"
         return response
 
     def vm_shutdown(self, request, context):
         print(f"received vm-shutdown for {request.vmName}")
         response = tira_host_pb2.Response()
-        response.commandId = tira_host_pb2.Response.Status.SUCCESS
+        if STATE.get('status') == "running":
+            response.status = tira_host_pb2.Response.Status.SUCCESS
+            STATE['status'] = 'stopped'
+        else:
+            response.status = tira_host_pb2.Response.Status.FAILED
+        response.commandId = "12345"
         return response
 
     def vm_snapshot(self, request, context):
         print(f"received vm-snapshot for {request.vmName}")
         response = tira_host_pb2.Response()
-        response.commandId = tira_host_pb2.Response.Status.SUCCESS
+        response.status = tira_host_pb2.Response.Status.FAILED
+        response.commandId = "12345"
         return response
 
     def vm_start(self, request, context):
         print(f"received vm-start for {request.vmName}")
         response = tira_host_pb2.Response()
-        response.commandId = tira_host_pb2.Response.Status.SUCCESS
+        if STATE.get('status') == "stopped":
+            response.status = tira_host_pb2.Response.Status.SUCCESS
+            STATE['status'] = 'running'
+        else:
+            response.status = tira_host_pb2.Response.Status.FAILED
+        response.commandId = "12345"
         return response
 
     def vm_stop(self, request, context):
         print(f"received vm-stop for {request.vmName}")
         response = tira_host_pb2.Response()
-        response.commandId = tira_host_pb2.Response.Status.SUCCESS
+        if STATE.get('status') in {"running", "sandboxed", "sandboxing"}:
+            response.status = tira_host_pb2.Response.Status.SUCCESS
+            STATE['status'] = 'stopped'
+        else:
+            response.status = tira_host_pb2.Response.Status.FAILED
+        response.commandId = "12345"
         return response
 
     def vm_unsandbox(self, request, context):
         print(f"received vm-unsandbox for {request.vmName}")
         response = tira_host_pb2.Response()
-        response.commandId = tira_host_pb2.Response.Status.SUCCESS
+        response.status = tira_host_pb2.Response.Status.SUCCESS
+        response.commandId = "12345"
         return response
 
     def run_execute(self, request, context):
         print(f"received run-execute for {request.submissionFile} - {request.inputDatasetName} - {request.inputRunPath} - {request.outputDirName} - {request.sandboxed} - {request.runId} - {request.snapshotName} - {request.optionalParameters}")
         response = tira_host_pb2.Response()
-        response.commandId = tira_host_pb2.Response.Status.SUCCESS
+        response.status = tira_host_pb2.Response.Status.SUCCESS
+        response.commandId = "12345"
         return response
 
     def run_eval(self, request, context):
         print(f"received run-eval for {request.submissionFile} - {request.inputDatasetName} - {request.inputRunPath} - {request.outputDirName} - {request.sandboxed} - {request.runId} - {request.snapshotName} - {request.optionalParameters}")
         response = tira_host_pb2.Response()
-        response.commandId = tira_host_pb2.Response.Status.SUCCESS
+        response.status = tira_host_pb2.Response.Status.SUCCESS
+        response.commandId = "12345"
         return response
 
 
