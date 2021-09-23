f1d592f403679b5bfd6b668c773c4df7e53391b8
Nikolay Kolyada <nikolay.kolyada@uni-weimar.de>
2021-09-23 15:01:36 +0200

    tira-host: remove command state file methods.

--- tira-host/src/tira_host/tira_model.py
+++ tira-host/src/tira_host/tira_model.py
@@ -26,89 +26,8 @@ class FileDatabase(object):
         self.command_logs_path.mkdir(exist_ok=True)
 
         self.vms = None  # dict of vm_id: modelpb.User
-        self.commandState = None
-
-        self._create_command_state_file()
 
         self._parse_vm_list()
-        self._parse_command_state()
-
-    def _parse_command_state(self):
-        """
-        Parse the command state file.
-        """
-        self.commandState = Parse(open(self.command_states_path, "r").read(), model_host.CommandState())
-
-    def _update_command_state_file(self):
-        with open(self.command_states_path, "w") as f:
-            f.write(MessageToString(self.commandState))
-
-    def _create_command_state_file(self):
-        """
-        Create state file for the current host. Check and rename if the file already exists.
-        """
-        if self.command_states_path.is_file() and self.command_states_path.stat().st_size > 0:
-            self.command_states_path.rename(
-                self.command_states_path.with_suffix(
-                    "." + datetime.strftime(datetime.now(), "%Y-%m-%dT%H:%M:%SZ") + ".prototext"))
-        self.command_states_path.touch()
-
-    def create_command(self, request, command_id, command_string):
-        """
-        Add new Command to the state file and create command log file.
-        :return: Command object
-        """
-        new_command = self.commandState.commands.add()
-        new_command.id = command_id
-        new_command.commandString = command_string
-        new_command.startTime = datetime.strftime(datetime.utcnow(), "%Y-%m-%dT%H:%M:%SZ")
-        new_command.status = model_host.Response.Status.RUNNING
-        if request.bulkCommandId:
-            new_command.bulkCommandId = request.bulkCommandId
-
-        # command_name = new_command.startTime + "_" + command_string.strip()[0]
-        log_file_path = self.command_logs_path / Path(command_id + ".log")
-        log_file_path.touch()
-        new_command.logFile = str(log_file_path)
-
-        self._update_command_state_file()
-
-        return new_command
-
-    def update_command(self, command_id, **kwargs):
-        """
-        Update command object with passed key-value pairs
-        :param command_id:
-        :param kwargs:
-        """
-        for c in self.commandState.commands:
-            if c.id == command_id:
-                for key, value in kwargs.items():
-                    setattr(c, key, value)
-        self._update_command_state_file()
-
-    # def update_command_log(self, command_id, output):
-
-    def remove_commands(self, commands_to_remove):
-        """
-        Remove finished commands
-        """
-        for c in self.commandState.commands:
-            if c.id in commands_to_remove:
-                self.commandState.commands.remove(c)
-
-        self._update_command_state_file()
-
-    def clean_command_state(self, time_ago):
-        """
-        Update command state file and remove commands older then 'time_ago'.
-        :param time_ago:
-        """
-        for command in self.commandState.commands:
-            if command.endTime and time.mktime(
-                    datetime.strptime(command.endTime, "%Y-%m-%dT%H:%M:%SZ").timetuple()) < time_ago:
-                self.commandState.commands.remove(command)
-        self._update_command_state_file()
 
     def _parse_vm_list(self):
         users = modelpb.Users()
