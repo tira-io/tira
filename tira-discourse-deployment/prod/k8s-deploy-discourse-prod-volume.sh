71547a588168d3e931b63373b496854296834747
Bastian Grahm <bastigrahm@gmx.de>
2021-11-23 11:43:26 +0100

    Delete tira-discourse-deployment directory

--- tira-discourse-deployment/prod/k8s-deploy-discourse-prod-volume.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Applying persistent volumes"
-kubectl apply -f discourse-prod-volumes.yml -n services-tira
--- tira-discourse-deployment/prod/k8s-undeploy-discourse-prod.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Deleting discourse-prod.yml"
-kubectl delete -f discourse-prod.yml -n services-tira || echo "discourse-prod.yml is already deleted"
