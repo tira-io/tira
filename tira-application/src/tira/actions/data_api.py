dc7a811aea845f6b8dec1b512dda906c72c23f45
Matti Wiegmann <matti.wiegmann@uni-weimar.de>
2021-12-07 18:18:47 +0100

    [application] refactor actions, now endpoints

--- tira-application/src/tira/actions/data_api.py
+++
@@ -1,9 +0,0 @@
-import logging
-from tira.forms import *
-from django.conf import settings
-
-include_navigation = True if settings.DEPLOYMENT == "legacy" else False
-
-logger = logging.getLogger("tira")
-logger.info("ajax_routes: Logger active")
-
