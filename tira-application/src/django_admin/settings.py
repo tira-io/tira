f73ed145956d3838b34ae45c88b840eac70cc8a4
Matti Wiegmann <matti.wiegmann@uni-weimar.de>
2021-12-14 14:30:56 +0100

    [application] add discourse secret location to config

--- tira-application/src/django_admin/settings.py
+++ tira-application/src/django_admin/settings.py
@@ -39,6 +39,7 @@ if not TIRA_ROOT.is_dir():
 TIRA_DB_PATH = Path(custom_settings.get("database", TIRA_ROOT / "state")) / "tira_vm_states.sqlite3"
 DEPLOYMENT = custom_settings.get("deployment", "legacy")
 LEGACY_USER_FILE = Path(custom_settings.get("legacy_users_file", TIRA_ROOT / "model" / "users" / "users.prototext"))
+DISRAPTOR_SECRET_FILE = Path(custom_settings.get("disraptor_secret_file", "/etc/discourse/client-api-key"))
 HOST_GRPC_PORT = custom_settings.get("host_grpc_port", "50051")
 APPLICATION_GRPC_PORT = custom_settings.get("application_grpc_port", "50052")
 GRPC_HOST = custom_settings.get("grpc_host", "local")  # can be local or remote
