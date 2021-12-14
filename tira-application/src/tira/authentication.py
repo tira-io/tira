87e37fa30536fbef5f94f04337e27f73066f6cc6
Matti Wiegmann <matti.wiegmann@uni-weimar.de>
2021-12-14 14:31:19 +0100

    [application] move group_create to auth

--- tira-application/src/tira/authentication.py
+++ tira-application/src/tira/authentication.py
@@ -1,11 +1,16 @@
 """
 
 """
+import json
 import logging
-from google.protobuf.text_format import Parse
-from .proto import TiraClientWebMessages_pb2 as modelpb
 import os
+from datetime import datetime
+
+import requests
 from django.conf import settings
+from google.protobuf.text_format import Parse
+
+from .proto import TiraClientWebMessages_pb2 as modelpb
 
 logger = logging.getLogger(__name__)
 
@@ -65,6 +70,9 @@ class Authentication(object):
 
     def logout(self, request, **kwargs):
         pass
+
+    def create_group(self, vm_id):
+        return {"status": 0, "message": f"create_group is not implemented for {self.__name__}"}
 
 
 class LegacyAuthentication(Authentication):
@@ -228,6 +236,50 @@ class DisraptorAuthentication(Authentication):
          """
         vms = self._get_user_groups(request)
         return vms[0] if len(vms) >= 1 else "no-vm-assigned"
+
+    def _discourse_api_key(self):
+        return open(settings.DISRAPTOR_SECRET_FILE, "r").read().strip()
+
+    def _create_discourse_group(self, vm):
+        group_bio = f"""Members of this group have access to the virtual machine ${vm.userName}:<br><br>
+    <ul>
+      <li>Host: {vm.host}</li>
+      <li>User: {vm.userName}</li>
+      <li>Passwort: {vm.userPw}</li>
+      <li>SSH Port: {vm.portSsh}</li>
+      <li>RDP Port: {vm.portRdp}</li>
+      <li>SSH Example: <code>sshpass -p {vm.userPw} ssh {vm.userName}@{vm.host} -p {vm.portSsh} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no</code></li>
+    </ul><br><br>
+    Please contact us when you have questions.
+    """
+
+        ret = requests.post("https://www.tira.io/admin/groups",
+                            headers={"Api-Key": self._discourse_api_key(), "Accept": "application/json",
+                                     "Content-Type": "multipart/form-data"},
+                            data={"group[name]": f"tira_vm_{vm.userName}", "group[visibility_level]": 2,
+                                  "group[members_visibility_level]": 2, "group[bio_raw]": group_bio}
+                            )
+
+        return json.loads(ret.text)['basic_group']['id']
+
+    def _create_discourse_invite_link(self, group_id):
+        ret = requests.post("https://www.tira.io/invites",
+                            headers={"Api-Key": self._discourse_api_key(), "Accept": "application/json",
+                                     "Content-Type": "multipart/form-data"},
+                            data={"group_ids[]": group_id, "max_redemptions_allowed": 20,
+                                  "expires_at": str(datetime.now().year + 1) + "-12-31"}
+                            )
+
+        return json.loads(ret.text)['link']
+
+    def create_group(self, vm_id):
+        vm_group = self._create_discourse_group(vm_id)
+        invite_link = self._create_discourse_invite_link(vm_group)
+
+        return {"status": 1,
+                "message": f"Invite Mail: Please use this link to create your login for TIRA: {invite_link}. "
+                           f"After login to TIRA, you can find the credentials and usage examples for your "
+                           f"dedicated virtual machine {vm_id} here: https://www.tira.io/g/tira_vm_{vm_id}"}
 
 
 auth = Authentication(authentication_source=settings.DEPLOYMENT,
