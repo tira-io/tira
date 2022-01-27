93cb0a8bd7b250a4c14f88fb1a315da18e3ee2e8
theelstner <theresa.elstner@uni-leipzig.de>
2022-01-27 14:19:06 +0100

    #99 Update log messages

--- tira-host/src/tira_scripts/tira-vm-rdp.sh
+++ tira-host/src/tira_scripts/tira-vm-rdp.sh
@@ -57,51 +57,61 @@ eval set -- "${FLAGS_ARGV}"
 #    Start rdp connetion to a vm.
 #
 main() {
-
+    
+    logInfo "[tira-vm-rdp] Checking Parameters..."
     # Print usage screen if wrong parameter count.
     if [ "$#" -eq 0 ]; then
-        logError "Missing arguments see:"
+        logError "[tira-vm-rdp] Wrong argument count, see:"
         usage
     fi
-
+    
+    logInfo "[tira-vm-rdp] Checking Parameters done."
+    
     vmname_or_user="$1"
 
     # Every parameter is parsed and extracted: now do the job.
+    logInfo "[tira-vm-rdp] Start building rdp connection to vm..."
+    
+    logInfo "[tira-vm-rdp] Extract vm info."
     vm_info=$(get_vm_info_from_tira "$vmname_or_user")
 
     if [ "$vm_info" = "" ]; then
-        logError "VM-Name/user $vmname_or_user is not registred. Use tira vm-list to get a list of all vms."
+        logError "[tira-vm-rdp] VM-Name/user $vmname_or_user is not registered. Use tira vm-list to get a list of all vms."
         exit 1
     fi
-    logDebug "Extracted vm info: \n$vm_info"
-
+    logInfo "[tira-vm-rdp] Extracted vm info: \n$vm_info"
+    
+    logInfo "[tira-vm-rdp] Set rdp connection parameters."
     admin="${FLAGS_admin}"
 
     if [ "$admin" = "${FLAGS_TRUE}" ]; then
         user=$(echo "$vm_info" | grep "adminName=" | sed "s|adminName=||g")
         pw=$(echo "$vm_info" | grep "adminPw=" | sed "s|adminPw=||g")
-        logDebug "User=Administrator"
+        logDebug "[tira-vm-rdp] User=Administrator"
     else
         user=$(echo "$vm_info" | grep "userName=" | sed "s|userName=||g")
         pw=$(echo "$vm_info" | grep "userPw=" | sed "s|userPw=||g")
-        logDebug "User= $user (default)"
+        logDebug "[tira-vm-rdp] User= $user (default)"
     fi
 
     port=$(echo "$vm_info" | grep "portRdp=" | sed "s|portRdp=||g")
     host=$(echo "$vm_info" | grep "host=" | sed "s|host=||g")
-    logDebug "with pw: $pw using port: $port on host: $host"
-
+    logDebug "[tira-vm-rdp] with pw: $pw using port: $port on host: $host"
+    logInfo "[tira-vm-rdp] Set rdp connection parameters done."
+    
+    logInfo "[tira-vm-rdp] Check VM state."
     # Reset vmname in order to make sure its the actual name, not just the user name.
     vmname=$(echo "$vm_info" | grep "vmName=" | sed "s|vmName=||g")
     # Check if vm is running:
     running=$(tira_call vm-info -r "$host" "$vmname" -s| grep "running")
 
     if [ "$running" = "" ]; then
-        logError "Vm $vmname is not running, therefore no rdp connection can be established."
+        logError "VM $vmname is not running, therefore no rdp connection can be established."
         exit 1
     fi
+    logInfo "[tira-vm-rdp] Check VM state done. VM is running."
 
-    logInfo "RDP connection: user: $user host: $host password: $pw"
+    logInfo "[tira-vm-rdp] Establish RDP connection: user: $user host: $host password: $pw"
     rdesktop "${host}:${port}" -u "$user" -p "$pw"
 
 }
