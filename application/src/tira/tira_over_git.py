8f3cfaf88d5a15709f6c83b9b65cb6d712941954
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2022-07-25 15:34:46 +0200

    Add run_evaluate_with_git_workflow which implements evaluations over git-pipelines via TIRA #238

--- application/src/tira/tira_over_git.py
+++ application/src/tira/tira_over_git.py
@@ -18,30 +18,42 @@ def repo_url(git_repository_id):
 def dict_to_gitlab_key_value_file(d):
     return '\n'.join([(k + '=' + v).strip() for (k,v) in d.items()])
 
+def __clone_repository_and_create_new_branch(repo_url, branch_name, directory):
+    repo = Repo.clone_from(, directory, branch='main')
+    repo.head.reference = repo.create_head(branch_name)
+        
+    return repo
+
+def __write_metadata_for_evaluation_job_to_repository(tmp_dir, task_id, transaction_id, dataset_id, vm_id, run_id, identifier):
+    job_dir = Path(tmp_dir) / dataset_id / vm_id / run_id
+    job_dir.mkdir(parents=True, exist_ok=True)
+    
+    with open(str(job_dir / 'job-to-execute.txt'), 'w') as f:
+        f.write(dict_to_gitlab_key_value_file({
+            # The pipeline executed first a pseudo software so the following three values are
+            # only dummy values so that the software runs successful.
+            'TIRA_IMAGE_TO_EXECUTE': 'ubuntu:18.04',
+            'TIRA_COMMAND_TO_EXECUTE': 'echo \'No software to execute. Only evaluation\'',
+            'TIRA_SOFTWARE_ID': '-1',
+
+            # The actual important stuff for the evaluator:
+            'TIRA_TASK_ID': task_id,
+            'TIRA_EVALUATOR_TRANSACTION_ID': transaction_id,
+            'TIRA_GIT_ID': identifier,
+        }))
+
+def __commit_and_push(repo, dataset_id, vm_id, run_id, identifier):
+    repo.index.add([str(Path(dataset_id) / vm_id / run_id / 'job-to-execute.txt')])
+    repo.index.commit("Evaluate software: " + identifier)
+    repo.remote().push(identifier)
+
 def run_evaluate_with_git_workflow(task_id, dataset_id, vm_id, run_id, transaction_id, evaluator_image, evaluator_command, git_repository_id):
     identifier = 'eval---' + dataset_id + '---' + vm_id + '---' + run_id + '---started-' + str(dt.now().strftime('%Y-%m-%d-%H-%M-%S'))
     
     with tempfile.TemporaryDirectory() as tmp_dir:
-        repo = Repo.clone_from(repo_url(git_repository_id), tmp_dir, branch='main')
-        repo.head.reference = repo.create_head(identifier)
-        job_dir = Path(tmp_dir) / dataset_id / vm_id / run_id
-        job_dir.mkdir(parents=True, exist_ok=True)
+        repo = __clone_repository_into_new_branch(repo_url(git_repository_id), identifier, tmp_dir)
         
-        with open(str(job_dir / 'job-to-execute.txt'), 'w') as f:
-            f.write(dict_to_gitlab_key_value_file({
-                # The pipeline executed first a pseudo software so the following three values are
-                # only dummy values so that the software runs successful.
-                'TIRA_IMAGE_TO_EXECUTE': 'ubuntu:18.04',
-                'TIRA_COMMAND_TO_EXECUTE': 'echo \'No software to execute. Only evaluation\'',
-                'TIRA_SOFTWARE_ID': '-1',
-                
-                # The actual important stuff for the evaluator:
-                'TIRA_TASK_ID': task_id,
-                'TIRA_EVALUATOR_TRANSACTION_ID': transaction_id,
-                'TIRA_GIT_ID': identifier,
-            }))
+        __write_metadata_for_evaluation_job_to_repository(tmp_dir, task_id, transaction_id, dataset_id, vm_id, run_id, identifier)
         
-        repo.index.add([str(Path(dataset_id) / vm_id / run_id / 'job-to-execute.txt')])
-        repo.index.commit("Evaluate software: " + identifier)
-        repo.remote().push(identifier)
+        __commit_and_push(repo, dataset_id, vm_id, run_id, identifier)
 
