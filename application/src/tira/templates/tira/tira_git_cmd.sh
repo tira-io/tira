79e60e924961272b4bb33ddf2eb89dfd16d67a4e
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2022-07-31 15:38:57 +0200

    add more details

--- application/src/tira/templates/tira/tira_git_cmd.sh
+++ application/src/tira/templates/tira/tira_git_cmd.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
 docker run \
-	-v ${PWD}:/repo \
 	-v /mnt/ceph/tira/:/mnt/ceph/tira/:ro \
 	-v ${HOME}/.ssh/id_rsa:${HOME}/.ssh/id_rsa:ro \
 	-v ${HOME}/.ssh/config:${HOME}/.ssh/config:ro \
@@ -16,5 +15,5 @@ docker run \
 	-e CI_SERVER_HOST={{ ci_server_host }} \
 	-e CI_PROJECT_ID={{ project_id }} \
 	--entrypoint ./src/tira_git.py \
-	--rm -ti webis/tira-git:0.0.16 ${@}
+	--rm -ti webis/tira-git:0.0.17 ${@}
 
