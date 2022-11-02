ee9c99efc3cbfef547e772e38feb41598bfebb1b
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2022-11-02 18:43:49 +0100

    mf

--- application/src/django_admin/settings.py
+++ application/src/django_admin/settings.py
@@ -247,7 +247,7 @@ GIT_USER_REPOSITORY_BRANCH = custom_settings.get('GIT_USER_REPOSITORY_BRANCH', '
 GIT_CONTAINER_REGISTRY_HOST = custom_settings.get('GIT_CONTAINER_REGISTRY_HOST', 'registry.webis.de')
 GIT_CI_AVAILABLE_RESOURCES = {
     'small-resources': {'cores': 1, 'ram': 10, 'gpu': 0, 'description': 'Small (1 CPU Cores, 10GB of RAM)', 'key': 'small-resources'},
-    'medium-resources': {'cores': 2, 'ram': 20, 'gpu': 0, 'description': 'Large (2 CPU Cores, 20GB of RAM)', 'key': 'medium-resources'},
+    'medium-resources': {'cores': 2, 'ram': 20, 'gpu': 0, 'description': 'Medium (2 CPU Cores, 20GB of RAM)', 'key': 'medium-resources'},
     'large-resources': {'cores': 4, 'ram': 40, 'gpu': 0, 'description': 'Large (4 CPU Cores, 40GB of RAM)', 'key': 'large-resources'},
     'small-resources-gpu': {'cores': 1, 'ram': 10, 'gpu': '1-nvidia-1080', 'description': 'Small w. GPU (1 CPU Cores, 10GB of RAM, 1 Nvidia GTX 1080 with 8GB)', 'key': 'small-resources-gpu'},
 }
