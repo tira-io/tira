47b2bce0929d5cd528c82b85c95473dd622e4a1e
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2022-12-11 13:18:26 +0100

    #336 Add first tests for participant.

---
+++ application/test/test_accessibility_of_all_endpoints_for_participant_user.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from api_access_matrix import access_matrix_for_user, PARTICIPANT
+from utils_for_testing import set_up_tira_environment, assert_all_url_patterns_are_tested, execute_method_behind_url_and_return_status_code
+from parameterized import parameterized
+
+
+class TestAccessibilityOfEndpointsForParticipantUser(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.tested_urls = []
+        set_up_tira_environment()
+
+    @parameterized.expand(access_matrix_for_user(PARTICIPANT))
+    def test_route(self, url_pattern, method_bound_to_url_pattern, request, expected_status_code, hide_stdout):
+        status_code = execute_method_behind_url_and_return_status_code(
+            method_bound_to_url_pattern,
+            request,
+            hide_stdout
+        )
+        
+        assert status_code == expected_status_code, \
+            f'Expected response for url_pattern {url_pattern} is {expected_status_code}. But I got {status_code}'
+
+        self.tested_urls += [url_pattern]
+
+    @classmethod
+    def tearDownClass(cls):
+        assert_all_url_patterns_are_tested(cls.tested_urls)    
+
