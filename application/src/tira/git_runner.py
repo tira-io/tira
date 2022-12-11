12f8e1764f861b42d2a1dff6cb241960bcd9e878
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2022-12-11 21:01:20 +0100

    Improve logging for git-integration

--- application/src/tira/git_runner.py
+++ application/src/tira/git_runner.py
@@ -40,6 +40,33 @@ def all_git_runners():
     return ret
 
 
+def check_that_git_integration_is_valid(namespace_url, private_token):
+    import tira.model as modeldb
+    from tira.tira_model import model
+    git_integration = {'namespace_url': namespace_url, 'private_token': private_token}
+    
+    try:
+        git_integration = modeldb.GitIntegration.objects.get(namespace_url=namespace_url)
+        git_integration = model._git_integration_to_dict(git_integration)
+        git_integration['private_token'] = private_token
+    except:
+        pass
+    
+    try:
+        git_runner = get_git_runner(git_integration)
+        
+        if not git_runner:
+            return (False, 'Invalid Parameters.')
+        
+        all_user_repositories = git_runner.all_user_repositories()
+        if all_user_repositories is not None and len(all_user_repositories) >= 0:
+            return (True, 'The git credentials are valid (tested by counting repositories).')
+        else:
+            return (False, 'The git credentials are not valid (tested by counting repositories).')
+    except Exception as e:
+        return (False, f'The Git credentials are not valid: {e}')
+
+
 def get_git_runner(git_integration):
     from tira.git_runner_integration import GitLabRunner, GithubRunner
 
@@ -47,7 +74,7 @@ def get_git_runner(git_integration):
         return None
 
     if 'github.com' in git_integration['namespace_url']:
-        return GithubRunner()
+        return GithubRunner(git_integration['private_token'])
     else:
         return GitLabRunner(
             git_integration['private_token'], git_integration['host'], git_integration['user_name'],
