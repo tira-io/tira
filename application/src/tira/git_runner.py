d09786b4758162889ef1b06edc02e18c70dff4f5
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2022-12-11 10:37:29 +0100

    #336 Add more unit tests and make improvements in the implementations

--- application/src/tira/git_runner.py
+++ application/src/tira/git_runner.py
@@ -42,6 +42,10 @@ def all_git_runners():
 
 def get_git_runner(git_integration):
     from tira.git_runner_integration import GitLabRunner, GithubRunner
+
+    if not git_integration or 'namespace_url' not in git_integration:
+        return None
+
     if 'github.com' in git_integration['namespace_url']:
         return GithubRunner()
     else:
