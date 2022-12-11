53f0343bb174c47f43917451bd33fd8c74251944
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2022-12-11 10:22:18 +0100

    #336 Add more unit tests for the admin user

--- application/test/run_all_tests.sh
+++ application/test/run_all_tests.sh
@@ -2,7 +2,13 @@
 
 rm -Rf tira-root/
 mkdir -p tira-root/model/virtual-machines/
+mkdir -p tira-root/model/virtual-machine-hosts
 mkdir -p tira-root/model/tasks/
+mkdir -p tira-root/model/users/
+mkdir -p tira-root/data/runs/dataset-1/example_participant/run-1
+touch tira-root/model/virtual-machines/virtual-machines.txt
+touch tira-root/model/virtual-machine-hosts/virtual-machine-hosts.txt
+touch tira-root/model/users/users.prototext
 
 PYTHONPATH=':../src:.' ../venv/bin/python3 ../src/manage.py test --settings=settings_test
 
