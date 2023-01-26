b12c0dc871219e02d010af9db877ed43e9d26b92
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2023-01-26 20:20:09 +0100

    prepare rest api client

---
+++ python-client/tira/rest_api_client.py
@@ -0,0 +1,40 @@
+import requests
+import json
+import pandas as pd
+
+class Client():
+    def __init__(self, api_key):
+        self.__api_key = api_key
+        self.fail_if_api_key_is_invalid()
+
+    def fail_if_api_key_is_invalid(self):
+        role = self.json_response('/api/role')
+        
+        if not role or 'status' not in role or 'role' not in role or 0 != role['status']:
+            raise ValueError('It seems like the api key is invalid. Got: ', role)
+
+    def submissions(self, task, dataset):
+        response = self.json_response(f'/api/submissions/{task}/{dataset}')
+        if 'status' not in response or 'context' not in response or response['status'] != 1:
+            raise ValueError('Got invalid response: ', json.dumps(response)[:150])
+        
+        response = response['context']
+        
+        ret = []
+        
+        for vm in response['vms']:
+            for run in vm['runs']:
+                for k,v in run['review'].items():
+                    run['review_' + k] = v
+                del run['review']
+
+                ret += [{**{'task': response['task_id'], 'dataset': response['dataset_id'], 'team': vm['vm_id']}, **run}]
+
+        return pd.DataFrame(ret)
+
+    def json_response(self, endpoint, params=None):
+        headers = {"Api-Key": self.__api_key, "Accept": "application/json"}
+        resp = requests.get(url='https://www.tira.io' + endpoint, headers=headers, params=params)
+        
+        return resp.json()
+
