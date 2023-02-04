a141c1082f05a138ba0ccd1e21e50ae99c7bb333
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2023-02-04 08:23:47 +0100

    Add utility method to load prototexts

--- python-client/tira/rest_api_client.py
+++ python-client/tira/rest_api_client.py
@@ -81,6 +81,32 @@ class Client():
     
         return target_dir + f'/{run_id}/output'
 
+    def __load_output(directory, evaluation=False, verbose=False):
+        files = glob(str(directory) + '/*' )
+    
+        if evaluation:
+            files = [i for i in files if i.endswith('.prototext')]
+    
+        if len(files) != 1:
+            raise ValueError('Expected exactly one output file. Got: ', files)
+    
+        files = files[0]
+    
+        if verbose:
+            print(f'Read file from {files}')
+    
+        if evaluation:
+            ret = {}
+            for i in [i for i in open(files, 'r').read().split('measure') if 'key:' in i and 'value:' in i]:
+                key = i.split('key:')[1].split('value')[0].split('"')[1]
+                value = i.split('key:')[1].split('value')[1].split('"')[1]
+            
+                ret[key.strip()] = __num(value.strip())
+            
+            return ret
+        else:
+            return pd.read_json(files, lines=True, orient='records')
+
     def json_response(self, endpoint, params=None):
         headers = {"Api-Key": self.__api_key, "Accept": "application/json"}
         resp = requests.get(url='https://www.tira.io' + endpoint, headers=headers, params=params)
