f7c4cf7dd63716d7effac03cfcdefda463d06529
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2023-02-04 08:43:44 +0100

    Add utility method to load prototexts

--- python-client/tira/rest_api_client.py
+++ python-client/tira/rest_api_client.py
@@ -81,7 +81,7 @@ class Client():
     
         return target_dir + f'/{run_id}/output'
 
-    def load_output_of_directory(directory, evaluation=False, verbose=False):
+    def load_output_of_directory(self, directory, evaluation=False, verbose=False):
         files = glob(str(directory) + '/*' )
     
         if evaluation:
