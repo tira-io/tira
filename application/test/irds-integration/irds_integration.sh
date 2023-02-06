4b0f7f8d1ad51ea1f594fc9695245c3638a443cb
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2023-02-06 08:44:29 +0100

    mf

--- application/test/irds-integration/irds_integration.sh
+++ application/test/irds-integration/irds_integration.sh
@@ -5,7 +5,7 @@ docker run --rm -ti \
     -v /mnt/ceph/tira/state/ir_datasets:/root/.ir_datasets \
     -v /mnt/ceph/tira/data/datasets/:/mnt/ceph/tira/data/datasets/ \
     -v ${PWD}/cw09-integration/:/tmp-out/ \
-    --entrypoint /irds_cli.sh webis/tira-application:0.0.34 \
+    --entrypoint /irds_cli.sh webis/tira-application:0.0.35 \
         --input_dataset_directory /mnt/ceph/tira/data/datasets/training-datasets/ir-benchmarks/clueweb09-en-trec-web-2009-20230107-training/ \
         --output_dataset_path $outputDir /tmp-out/web-09-rerank --rerank /tmp-out/web-09-run
 
