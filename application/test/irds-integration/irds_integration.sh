43881247a22942f852d701e5af48eb10bc780127
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2023-02-06 10:33:30 +0100

    Improve irds integration: We now break score ties etc.

--- application/test/irds-integration/irds_integration.sh
+++ application/test/irds-integration/irds_integration.sh
@@ -2,6 +2,9 @@
 
 docker pull webis/tira-application:0.0.35
 
+rm -Rf cw09-integration/web-09-rerank
+rm -Rf cranfield-integration/cranfield-rerank
+
 docker run --rm -ti \
     -v /mnt/ceph/storage/corpora/corpora-thirdparty:/mnt/ceph/storage/corpora/corpora-thirdparty:ro \
     -v /mnt/ceph/tira/state/ir_datasets:/root/.ir_datasets \
@@ -10,4 +13,13 @@ docker run --rm -ti \
     --entrypoint /irds_cli.sh webis/tira-application:0.0.35 \
         --input_dataset_directory /mnt/ceph/tira/data/datasets/training-datasets/ir-benchmarks/clueweb09-en-trec-web-2009-20230107-training/ \
         --output_dataset_path $outputDir /tmp-out/web-09-rerank --rerank /tmp-out/web-09-run
+
+docker run --rm -ti \
+    -v /mnt/ceph/storage/corpora/corpora-thirdparty:/mnt/ceph/storage/corpora/corpora-thirdparty:ro \
+    -v /mnt/ceph/tira/state/ir_datasets:/root/.ir_datasets \
+    -v /mnt/ceph/tira/data/datasets/:/mnt/ceph/tira/data/datasets/ \
+    -v ${PWD}/cranfield-integration/:/tmp-out/ \
+    --entrypoint /irds_cli.sh webis/tira-application:0.0.35 \
+        --input_dataset_directory /mnt/ceph/tira/data/datasets/training-datasets/ir-benchmarks/clueweb09-en-trec-web-2009-20230107-training/ \
+        --output_dataset_path $outputDir /tmp-out/cranfield-rerank --rerank /tmp-out/cranfield-run
 
