b7ff1643cf62033c15bb18f36fd14cb25bc1ff85
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2023-02-25 22:15:39 +0100

    mf

--- python-client/tira/pyterrier_util.py
+++ python-client/tira/pyterrier_util.py
@@ -1,6 +1,8 @@
 from pyterrier.transformer import Transformer
 import json
 import pandas as pd
+import tempfile
+import gzip
 
 
 class TiraRerankingTransformer(Transformer):
@@ -37,4 +39,46 @@ class TiraRerankingTransformer(Transformer):
             keeping = topics.columns[~ topics.columns.isin(drop_columns)]
 
         return topics[keeping].merge(df, how='left', left_on=["qid", "docno"], right_on=["qid", "docno"])
+
+class TiraLocalExecutionRerankingTransformer(Transformer):
+    """
+    A Transformer that re-execues software submitted in TIRA.
+    """
+
+    def __init__(self, approach, tira_client, **kwargs):
+        self.task, self.team, self.software = approach.split('/')
+        self.tira_client = tira_client
+
+    def transform(self, topics):
+        import numpy as np
+        assert "qid" in topics.columns
+        
+        with tempfile.TemporaryDirectory() as tmp_directory:
+            os.makedirs(tmp_directory + '/input')
+            os.makedirs(tmp_directory + '/output')
+            
+            with gzip.open(tmp_directory + '/input/re-rank.jsonl.gz', 'w') as f:
+                for _, i in topics.iterrows():
+                    f.write(json.dumps(i) + '\n')
+            
+            print([json.reads(i) for i in gzip.open(tmp_directory + '/input/re-rank.jsonl.gz', 'r')])
+        
+#        df = []
+#        for tira_configuration in tira_configurations:
+#            df += [self.tira_client.download_run(tira_configuration['tira_task'], tira_configuration['tira_dataset'], self.software, self.team, tira_configuration['tira_first_stage_run_id'])]
+#        df = pd.concat(df)
+#        df['qid'] = df['query'].astype(str)
+#        df['docno'] = df['docid'].astype(str)
+#        del df['query']
+#        del df['docid']
+#
+#        common_columns = np.intersect1d(topics.columns, df.columns)
+#
+#        # we drop columns in topics that exist in the df
+#        keeping = topics.columns
+#        drop_columns = [i for i in common_columns if i not in {"qid", "docno"}]
+#        if len(drop_columns) > 0:
+#            keeping = topics.columns[~ topics.columns.isin(drop_columns)]
+#
+#        return topics[keeping].merge(df, how='left', left_on=["qid", "docno"], right_on=["qid", "docno"])
 
