5c378853426c99b0b97036f683053b33a6873ea4
Nicolas Handke <nicolas.handke@gmx.de>
2023-03-16 13:40:14 +0100

    Change dir names in verbose for TIRA CLI command

--- python-client/tira/local_execution_integration.py
+++ python-client/tira/local_execution_integration.py
@@ -23,22 +23,22 @@ class LocalExecutionIntegration():
     
         return cmd
 
+    def __normalize_path(self, path):
+        beheaded_path = path if not str(path).startswith('$PWD') else str(path)[4:]
+        beheaded_path = os.path.abspath(".") if len(beheaded_path) == 0 or beheaded_path in ['/', '.'] else \
+            beheaded_path[0].replace('/', '') + beheaded_path[1:]
+        return beheaded_path
+
     def construct_verbosity_output(self, input_dir, output_dir, image, command, original_args):
-        tira_run_input_dir = input_dir if not str(input_dir) == str(os.path.abspath(".")) else None
-        tira_run_output_dir = output_dir if not str(output_dir) == str(os.path.abspath("tira-output")) else None
+        beheaded_input_dir = self.__normalize_path(input_dir)
+        beheaded_output_dir = self.__normalize_path(output_dir)
+        tira_run_input_dir = beheaded_input_dir if not str(beheaded_input_dir) == str(os.path.abspath(".")) else None
+        tira_run_output_dir = beheaded_output_dir if not str(beheaded_output_dir) == str(os.path.abspath("tira-output")) else None
         tira_run_python_args = {k: v for k, v in original_args.items()}
         if type(tira_run_python_args['input_dir']) is str and tira_run_python_args['input_dir'].startswith('$PWD'):
-            tail = tira_run_python_args['input_dir'][4:]
-            if len(tail) == 0 or tail == '/' or tail == os.sep:
-                tira_run_python_args['input_dir'] = "./"
-            elif tail.startswith('/') or tail.startswith(os.sep):
-                tira_run_python_args['input_dir'] = tail[1:]
+            tira_run_python_args['input_dir'] = self.__normalize_path(tira_run_python_args['input_dir'])
         if type(tira_run_python_args['output_dir']) is str and tira_run_python_args['output_dir'].startswith('$PWD'):
-            tail = tira_run_python_args['output_dir'][4:]
-            if len(tail) == 0 or tail == '/' or tail == os.sep:
-                tira_run_python_args['output_dir'] = "./"
-            elif tail.startswith('/') or tail.startswith(os.sep):
-                tira_run_python_args['output_dir'] = tail[1:]
+            tira_run_python_args['output_dir'] = self.__normalize_path(tira_run_python_args['output_dir'])
         tira_run_args = [(k, v if type(v) is not str else f'"{v}"') for k, v in tira_run_python_args.items()]
         tira_run_args = [i.strip() for i in ['' if not tira_run_python_args[k] else f'{k}={v}' for k, v in tira_run_args] if i]
     
