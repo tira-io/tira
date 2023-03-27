42eb66284d96e1c5c569249348e43e7594552db5
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2023-03-27 16:27:48 +0200

    Allow REST Calls for organizers. This implements #339 .

--- application/src/tira/endpoints/admin_api.py
+++ application/src/tira/endpoints/admin_api.py
@@ -402,7 +402,7 @@ def admin_add_organizer(request, organizer_id):
 
         if add_default_git_integrations:
             git_integrations = [model.model.get_git_integration(settings.DEFAULT_GIT_INTEGRATION_URL, '<OMMITTED>')]
-            model.edit_organizer(organizer_id, data["name"], data["years"], data["web"], data['gitUrlToNamespace'],
+            model.model.edit_organizer(organizer_id, data["name"], data["years"], data["web"], data['gitUrlToNamespace'],
                                  data['gitPrivateToken'], git_integrations=git_integrations)
 
 
