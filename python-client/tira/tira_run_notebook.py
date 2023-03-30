ebf74840ce8d93606386233b1fdcc0bee5ca0e34
Nicolas Handke <nicolas.handke@gmx.de>
2023-03-30 13:26:35 +0200

    Add command tira-run-notebook for ipynb execution in TIRA

---
+++ python-client/tira/tira_run_notebook.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import sys
+import subprocess
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog='tira-run-notebook', description='')
+
+    parser.add_argument('--input', type=str, help='The directory that contains the input data (within tira-run this should be \'$inputDataset\').', required=True)
+    parser.add_argument('--notebook', type=str, help='The notebook to execute.', required=True)
+    parser.add_argument('--output', type=str, help='The directory for storing output data (within tira-run this should be \'$outputDir\').', required=True)
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    os.environ['TIRA_INPUT_DIRECTORY'] = args.input
+    os.environ['TIRA_OUTPUT_DIRECTORY'] = args.output
+
+    command = f'runnb --allow-not-trusted {args.notebook}'
+    subprocess.check_call(command, shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
