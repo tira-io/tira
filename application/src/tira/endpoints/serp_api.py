7afa7ca83a0dd688408323188d949d9c70eefdfb
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2023-05-01 18:45:07 +0200

    mf

--- application/src/tira/endpoints/serp_api.py
+++ application/src/tira/endpoints/serp_api.py
@@ -28,8 +28,9 @@ def serp(request, context, vm_id, dataset_id, task_id, run_id):
                        vm_id / run_id / 'output' / 'run.txt'
             serp_file = Path(settings.TIRA_ROOT) / "state" / "serp" / "version-0.0.1" / "runs" / dataset_id / vm_id / run_id / "serp.html"
             serp_dir = (serp_file / "..").resolve()
+            run_dir = (run_file / "..").resolve()
             Path.mkdir(serp_dir, parents=True, exist_ok=True)
-            
+
             if not run_file.is_file():
                 raise ValueError(f'Error: The expected file {run_file} does not exist.')
 
@@ -40,8 +41,9 @@ def serp(request, context, vm_id, dataset_id, task_id, run_id):
             image = model.get_dataset(run['dataset'])['irds_docker_image']
             command = [
                 ['sudo', 'podman', '--storage-opt', 'mount_program=/usr/bin/fuse-overlayfs', 'run',
+                 '-v', f':{run_dir}/run_dir:ro',
                  '-v', f'{serp_dir}:/output-tira-tmp/',
-                 '--entrypoint', 'sh', image, '-c', f'diffir --dataset {irds_id} --web $outputDir/run.txt > /tmp/run.html && mv /tmp/run.html /output-tira-tmp/serp.html']
+                 '--entrypoint', 'sh', image, '-c', f'diffir --dataset {irds_id} --web /run_dir/run.txt > /tmp/run.html && mv /tmp/run.html /output-tira-tmp/serp.html']
             ]
 
             descriptions = ['### RUN diffir']
