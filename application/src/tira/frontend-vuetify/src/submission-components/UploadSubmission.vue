531a3c3c8e160b7b347c3860a6f4a80a0a7cd085
Luca Thale-Bombien <lthale-bombien@web.de>
2023-05-25 13:11:13 +0200

    Issue #428 Added Upload Comoponents for run, docker and vm submissions

--- application/src/tira/frontend-vuetify/src/submission-components/UploadSubmission.vue
+++ application/src/tira/frontend-vuetify/src/submission-components/UploadSubmission.vue
@@ -1,12 +1,12 @@
 <template>
-<v-card tonal class="my-10">
-          <v-card-item>
-            <v-btn variant="outlined" @click="showUploadForm=false; uploadgroupselected=''; editUploadMetadataToggle=false;">Add new upload group</v-btn>
-            <v-btn variant="outlined" v-for="uploadGroup in this.all_uploadgroups"
-                    @click="uploadgroupselected=uploadGroup ;showUploadForm=true; editUploadMetadataToggle=false;">
-         {{ uploadGroup.display_name }} </v-btn>
-          </v-card-item>
-          <v-card-item v-if="!showUploadForm">
+<v-tabs v-model="tab"
+    fixed-tabs class="my-10">
+            <v-tab value="newUploadGroup" variant="outlined" @click="uploadgroupselected='';">Add new upload group</v-tab>
+            <v-tab variant="outlined" v-for="uploadGroup in this.all_uploadgroups" :value="uploadGroup.display_name">
+         {{ uploadGroup.display_name }} </v-tab>
+  </v-tabs>
+  <v-window v-model="tab">
+          <v-window-item value="newUploadGroup">
             <h2>Create Run Upload Group</h2>
               <p>Please click on "Add Upload Group" below to create a new run upload group.</p>
               <p>Please use one upload group (you can edit the metadata of an upload group later) per appraoch. I.e., in TIRA, you can usually run software submissions on different datasets. For manually uploaded runs, we employ the same methodology: Please create one run upload group per approach, so that you can upload "executions" of the same approach on different datasets while maintaining the documentation.</p>
@@ -16,9 +16,10 @@
               <v-btn variant="outlined" @click="addUpload()">
                   Add Upload Group
               </v-btn>
-          </v-card-item>
-          <v-card-item v-if="showUploadForm">
-            <v-card-actions class="d-flex justify-lg-space-between">
+          </v-window-item>
+          <v-window-item v-for="uploadGroup in this.all_uploadgroups" :value="uploadGroup.display_name">
+            <div class="d-flex justify-lg-space-between">
+              <h2>{{ uploadGroup.display_name }}</h2>
               <p>Please add a description that describes uploads of this type.</p>
               <div>
               <v-btn variant="outlined" color="#303f9f"><v-icon>mdi-file-edit-outline</v-icon>Edit</v-btn>
@@ -27,7 +28,7 @@
                 location="bottom"
               >Attention! This deletes the container and ALL runs associated with it</v-tooltip><v-icon>mdi-delete-alert-outline</v-icon>Delete</v-btn>
               </div>
-            </v-card-actions>
+            </div>
             <v-form>
               <v-file-input
                 accept=""
@@ -45,17 +46,16 @@
 
             <v-btn :disabled="uploading || fileHandle === null || uploadDataset === 'None'"
                    @click="fileUpload()">Upload Run</v-btn>
-          </v-card-item>
-        </v-card>
+          </v-window-item>
+    </v-window>
 </template>
 
 <script>
 export default {
   name: "UploadSubmission",
-  props: ['csrf', 'datasets', 'upload', "taskid", "userid", "running_evaluations"],
-  emits: ['addNotification', 'pollEvaluations', 'removeRun', 'addUploadgroup'],
   data () {
       return {
+        tab: null,
         showUploadForm: false,
         uploadDataset: '',
         uploadFormError: '',
@@ -63,7 +63,10 @@ export default {
         uploading: false,
         uploadgroupselected: null,
         editUploadMetadataToggle: false,
-        all_uploadgroups: [],
+        all_uploadgroups: [
+                    {"display_name": 'spiky-dandelion'},
+                    {"display_name": 'snobby-pup'},
+                    {"display_name": 'dry-heaven'}],
         selectedDataset: '',
         datasets: [{
                 "dataset_id": "1",
@@ -92,132 +95,11 @@ export default {
       }
   },
   methods: {
-        async get(url) {
-            const response = await fetch(url)
-            if (!response.ok) {
-                throw new Error(`Error fetching endpoint: ${url} with ${response.status}`);
-            }
-            let results = await response.json()
-            if (results.status === 1) {
-                throw new Error(`${results.message}`);
-            }
-            return results
-        },
-        deleteUpload() {
-            this.get(`/task/${this.taskid}/vm/${this.userid}/upload-delete/${this.uploadgroupselected.id}`)
-                .then(message => {
-                    this.all_uploadgroups = this.all_uploadgroups.filter(i => i.id != this.uploadgroupselected.id)
-                    this.uploadgroupselected = null
-                    this.showUploadForm = false
-                })
-                .catch(error => {
-                    console.log(error)
-                    this.$emit('addNotification', 'error', error.message)
-                })
-        },
-        async editUploadGroup() {
-            let params = {
-                "display_name": this.uploadgroupselected.display_name,
-                "description": this.uploadgroupselected.description,
-                "paper_link": this.uploadgroupselected.paper_link
-            }
-
-            submitPost(
-                `/task/${this.taskid}/vm/${this.userid}/save_software/upload/${this.uploadgroupselected.id}`,
-                this.csrf,
-                params
-            ).then(message => {
-                for (let uid in this.all_uploadgroups) {
-                    if (this.all_uploadgroups[uid].id === this.uploadgroupselected.id) {
-                        this.all_uploadgroups[uid].display_name = this.uploadgroupselected.display_name
-                        this.all_uploadgroups[uid].description = this.uploadgroupselected.description
-                        this.all_uploadgroups[uid].paper_link = this.uploadgroupselected.paper_link
-                    }
-                }
-
-                this.editUploadMetadataToggle = false
-            }).catch(error => {
-                this.$emit('addNotification', 'error', `Editing failed with ${error}`)
-            })
-        },
-        addUpload() {
-            this.showUploadForm=true
-            this.get(`/task/${this.taskid}/vm/${this.userid}/add_software/upload`).then(message => {
-                const new_uploadgroup = {"uploadgroup":
-                    {"display_name": message.context.upload.display_name}}
-                this.$emit('addUploadgroup', new_uploadgroup)
-                this.uploadgroupselected = message.context.upload
-                this.all_uploadgroups.push(this.uploadgroupselected)
-            })
-            .catch(error => {
-                this.$emit('addNotification', 'error', error.message)
-            })
-        },
-        async fileUpload() {  // async
-            console.log(this.uploading, this.uploadDataset, this.fileHandle)
-            this.uploading = true
-            let formData = new FormData();
-            const headers = new Headers({'X-CSRFToken': this.csrf})
-            formData.append("file", this.fileHandle);
-            const response = await fetch(`/task/${this.taskid}/vm/${this.userid}/upload/${this.uploadDataset}/${this.uploadgroupselected.id}`, {
-              method: "POST",
-              headers,
-              body: formData
-            });
-
-            let r = await response.json()
-            if (!response.ok) {
-                this.$emit('addNotification', 'error', `Uploading failed with status ${response.status}: ${await response.text()}`)
-            } else if (r.status !== 0){
-                this.uploadFormError = 'Error: ' + r.message
-            } else {
-                this.uploadFormError = ''
-                console.log(r.new_run)
-
-                //TODO: this is a fast hack as there are currently side effects with the run-list (duplicated runs that appear too late)
-                window.location.reload()
-
-                let new_run = r.new_run.run
-                new_run.review.published = false
-
-                this.uploadgroupselected.runs.push(new_run)
-
-                for (let uid in this.all_uploadgroups) {
-                    if (this.all_uploadgroups[uid].id === this.uploadgroupselected.id) {
-                        this.all_uploadgroups[uid].runs.push(new_run)
-                    }
-                }
-
-                this.fileHandle = null
-                this.upload.last_edit = r.last_edit_date
-                if (r.started_evaluation) {
-                    this.$emit('pollEvaluations')
-                }
-            }
-            this.$refs.file.value = null
-            this.uploading = false
-        },
-        saveFileRef() {
-            this.fileHandle = this.$refs.file.files[0];
-        },
+    addUpload() {
+      const new_uploadgroup = {"display_name": 'new-upload-group_' + Math.floor(Math.random() * 1000)};
+      this.all_uploadgroups.push(new_uploadgroup);
+      this.tab = new_uploadgroup.display_name;
     },
-    computed: {
-        datasetOptions() {
-            if (!this.datasets) {
-                return []
-            }
-            return this.datasets.filter(k => !k.is_deprecated).map(k => {
-                return [k.dataset_id, k.display_name]
-            })
-        },
-        filterUploadRuns() {
-            return this.upload.runs.filter(k => !k.is_evaluation).map(k => {
-                return [k.display_name]
-            })
-        }
-    },
-    beforeMount() {
-        this.all_uploadgroups = this.upload;
-    }
+  }
 }
 </script>
