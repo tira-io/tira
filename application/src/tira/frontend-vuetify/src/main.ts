0d334417300a6368d10c91675c839db7d3a101ac
Maik Fröbe <maik.froebe@uni-jena.de>
2023-06-13 13:31:03 +0200

    Improve layout #428.

--- application/src/tira/frontend-vuetify/src/main.ts
+++ application/src/tira/frontend-vuetify/src/main.ts
@@ -18,28 +18,45 @@ import { createRouter,createWebHistory} from 'vue-router'
 // Plugins
 import { registerPlugins } from '@/plugins'
 
-const routes = [
-  {path: '/', component: Home},
-  {path: '/tasks', component: Tasks},
-  {path: '/task-overview/:task_id?/:dataset_id?', component: TaskOverview},
-  {path: '/run-upload', component: RunUpload},
-  // TODO: Temporary additional routes for transition form previous TIRA UI version.
-  {path: '/frontend-vuetify/landing', component: Home},
-  {path: '/frontend-vuetify/tasks', component: Tasks},
-  {path: '/frontend-vuetify/task-overview/:task_id?/:dataset_id?', component: TaskOverview},
-  {path: '/frontend-vuetify/run-upload', component: RunUpload},
+export default function register_app() {
+  const app_selector = '#app'
 
-  // Fallback: everything matches to home.
-  { path: '/:pathMatch(.*)*', component: Home },
-]
-const router = createRouter({
-  history: createWebHistory(),
-  routes: routes,
-})
+  const app_elem = document.querySelector(app_selector)
+  if (app_elem && '__vue_app__' in app_elem && app_elem.__vue_app__) {
+    console.log('TIRA app is already mounted.')
+    return;
+  }
 
-const app = createApp(App)
-app.use(router)
+  console.log('Mount TIRA vue app to location: ' + window.location)
+  const routes = [
+    {path: '/', component: Home},
+    {path: '/tasks', component: Tasks},
+    {path: '/task-overview/:task_id?/:dataset_id?', component: TaskOverview},
+    {path: '/run-upload', component: RunUpload},
+    // TODO: Temporary additional routes for transition form previous TIRA UI version.
+    {path: '/frontend-vuetify/landing', component: Home},
+    {path: '/frontend-vuetify/tasks', component: Tasks},
+    {path: '/frontend-vuetify/task-overview/:task_id?/:dataset_id?', component: TaskOverview},
+    {path: '/frontend-vuetify/run-upload', component: RunUpload},
 
-registerPlugins(app)
+    // Fallback: everything matches to home.
+    { path: '/:pathMatch(.*)*', component: Home },
+  ]
+  const router = createRouter({
+    history: createWebHistory(),
+    routes: routes,
+  })
 
-app.mount('#app')
+  const app = createApp(App)
+  app.use(router)
+
+  registerPlugins(app)
+  app.mount(app_selector)
+}
+
+declare global { interface Window { register_app: any; }}
+window.register_app = register_app;
+
+register_app()
+
+
