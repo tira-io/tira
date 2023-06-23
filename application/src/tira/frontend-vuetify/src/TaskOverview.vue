cc2e5218b4f686a75234b75d1f699da9b53ae9a3
juhehehe <julius.hehenkamp@gmail.com>
2023-06-23 17:23:12 +0200

    slightly changed TaskOverview.vue regarding navigation

--- application/src/tira/frontend-vuetify/src/TaskOverview.vue
+++ application/src/tira/frontend-vuetify/src/TaskOverview.vue
@@ -1,69 +1,112 @@
 <template>
+  <v-container>
   <tira-breadcrumb/>
   <loading :loading="loading"/>
-
   <v-container v-if="!loading">
-  <v-card class="px2" max-width="2560">
-    <v-card-item :title="task.task_name">
-      <template v-slot:subtitle>by <a :href="link_organizer">{{ task.organizer }}</a> (<a :href="contact_organizer">contact</a>)</template>
-    </v-card-item>
+    <v-card >
+      <v-card-actions>
+        <v-layout class="overflow-visible " style="height: 56px; ">
+          <v-bottom-navigation v-model="tab" color="primary" :elevation="10" grow>
 
-    <v-card-text class="py-0">
-      <v-row no-gutters><v-col cols="10">{{task.task_description}}</v-col></v-row>
-    </v-card-text>
+             <v-btn value="Overview">
+              <v-icon>mdi-home-analytics</v-icon>
+              Overview
+            </v-btn>
 
-    <div class="d-flex py-3">
-      <v-list-item density="compact" prepend-icon="mdi-calendar-blank-outline">
-        <v-list-item-subtitle>{{task.year}}</v-list-item-subtitle>
-        <v-tooltip activator="parent" location="bottom">Task year(s): {{task.year}}</v-tooltip>
-      </v-list-item>
+            <v-btn value="Leaderboard">
+              <v-icon>mdi-google-analytics</v-icon>
+              Leaderboard
+            </v-btn>
 
-      <v-list-item density="compact" prepend-icon="mdi-account-group">
-        <v-list-item-subtitle>{{task.teams}}</v-list-item-subtitle>
-        <v-tooltip activator="parent" location="bottom">Teams: {{ task.teams }}</v-tooltip>
-      </v-list-item>
+            <v-btn value="Submission">
+              <v-icon>mdi-new-box</v-icon>
+              New Submission
+            </v-btn>
 
-      <v-list-item density="compact" prepend-icon="mdi-database-outline">
-        <v-list-item-subtitle>{{task.dataset_count}}</v-list-item-subtitle>
-        <v-tooltip activator="parent" location="bottom">Dataset count: {{task.dataset_count}}</v-tooltip>
-      </v-list-item>
-      
-      <v-list-item density="compact" prepend-icon="mdi-briefcase-outline">
-        <v-list-item-subtitle>{{task.software_count}}</v-list-item-subtitle>
-        <v-tooltip activator="parent" location="bottom">Software count: {{task.software_count}}</v-tooltip>
-      </v-list-item>
-    </div>
+          </v-bottom-navigation>
+        </v-layout>
+      </v-card-actions>
+    </v-card>
 
-    <v-divider></v-divider>
 
-    <v-card-actions>
-      <v-row>
-        <v-col cols="6"><submit-button :task="task" :vm="vm" :user_id="user_id" :user_vms_for_task="user_vms_for_task"/></v-col>
-        <v-col cols="6"><v-btn variant="outlined" :href="task.web" block>Task Website</v-btn></v-col>
-      </v-row>
-    </v-card-actions>
-  </v-card>
+    <v-window v-model="tab">
+
+      <v-window-item value="Overview">
+        <loading :loading="loading"/>
+        <v-container v-if="!loading">
+          <v-card class="px2" max-width="2560">
+            <v-card-item :title="task.task_name">
+              <template v-slot:subtitle>by <a :href="link_organizer">{{ task.organizer }}</a> (<a :href="contact_organizer">contact</a>)</template>
+            </v-card-item>
+
+             <v-card-text class="py-0">
+               <v-row no-gutters><v-col cols="10">{{task.task_description}}</v-col></v-row>
+             </v-card-text>
+
+            <div class="d-flex py-3">
+              <v-list-item density="compact" prepend-icon="mdi-calendar-blank-outline">
+                <v-list-item-subtitle>{{task.year}}</v-list-item-subtitle>
+                <v-tooltip activator="parent" location="bottom">Task year(s): {{task.year}}</v-tooltip>
+              </v-list-item>
+
+              <v-list-item density="compact" prepend-icon="mdi-account-group">
+                <v-list-item-subtitle>{{task.teams}}</v-list-item-subtitle>
+                <v-tooltip activator="parent" location="bottom">Teams: {{ task.teams }}</v-tooltip>
+              </v-list-item>
+
+              <v-list-item density="compact" prepend-icon="mdi-database-outline">
+                <v-list-item-subtitle>{{task.dataset_count}}</v-list-item-subtitle>
+                <v-tooltip activator="parent" location="bottom">Dataset count: {{task.dataset_count}}</v-tooltip>
+              </v-list-item>
+
+              <v-list-item density="compact" prepend-icon="mdi-briefcase-outline">
+                <v-list-item-subtitle>{{task.software_count}}</v-list-item-subtitle>
+                <v-tooltip activator="parent" location="bottom">Software count: {{task.software_count}}</v-tooltip>
+              </v-list-item>
+            </div>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-row>
+              <v-col cols="6"><submit-button :task="task" :vm="vm" :user_id="user_id" :user_vms_for_task="user_vms_for_task"/></v-col>
+              <v-col cols="6"><v-btn variant="outlined" :href="task.web" block>Task Website</v-btn></v-col>
+            </v-row>
+          </v-card-actions>
+        </v-card>
+        </v-container>
+      </v-window-item>
+
+      <v-window-item value="Leaderboard">
+
+        <tira-task-admin v-if="!loading"/>
+          <v-container v-if="!loading" id="dataset-select">
+            <h2>Submissions</h2>
+            <v-autocomplete label="Dataset" :items="datasets" item-title="display_name" item-value="dataset_id"
+                            v-model="selectedDataset" variant="underlined" clearable/>
+
+            <run-list v-if="selectedDataset" :task_id="task_id" :organizer="task.organizer" :organizer_id="task.organizer_id" :dataset_id="selectedDataset"/>
+          </v-container>
+
+      </v-window-item>
+
+      <v-window-item value="Submission">
+        <RunUpload/>
+      </v-window-item>
+
+    </v-window>
   </v-container>
-
-  <tira-task-admin v-if="!loading"/>
-
-  <v-container v-if="!loading" id="dataset-select">
-    <h2>Submissions</h2>
-    <v-autocomplete label="Dataset" :items="datasets" item-title="display_name" item-value="dataset_id"
-                    v-model="selectedDataset" variant="underlined" clearable/>
-
-    <run-list v-if="selectedDataset" :task_id="task_id" :organizer="task.organizer" :organizer_id="task.organizer_id" :dataset_id="selectedDataset"/>
   </v-container>
 </template>
 
 <script lang="ts">
   import { TiraBreadcrumb, TiraTaskAdmin, RunList, Loading, SubmitButton } from './components'
+  import RunUpload from "@/RunUpload.vue";
   import { VAutocomplete } from 'vuetify/components'
   import { extractTaskFromCurrentUrl, get_link_to_organizer, get_contact_link_to_organizer, extractDatasetFromCurrentUrl, changeCurrentUrlToDataset, get, inject_response, reportError, extractRole} from './utils'
-
   export default {
     name: "task-list",
-    components: {TiraBreadcrumb, RunList, Loading, VAutocomplete, SubmitButton, TiraTaskAdmin},
+    components: {TiraBreadcrumb, RunList, Loading, VAutocomplete, SubmitButton, TiraTaskAdmin, RunUpload},
     data() {
       return {
         task_id: extractTaskFromCurrentUrl(),
@@ -78,6 +121,7 @@
         user_id: '',
         user_vms_for_task: [],
         datasets: [{'dataset_id': 'loading...', 'display_name': 'loading...'}],
+        tab: "test",
     }
   },
   computed: {
