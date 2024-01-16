cee5f8d370c103fc655a20c7669bff5dcca9f509
kavlahkaff <de28oser@studserv.uni-leipzig.de>
2024-01-16 13:51:52 +0000

    added vuetify stepper, but components still doubled

--- application/src/tira/frontend-vuetify/src/submission-components/NewDockerSubmission.vue
+++ application/src/tira/frontend-vuetify/src/submission-components/NewDockerSubmission.vue
@@ -1,8 +1,131 @@
 <template>
-  <div style="min-height: 120vh;">
+  
   <import-submission submission_type="docker"/>
   <h2>Create New Docker Software</h2>
-  <div class="d-flex justify-lg-space-between">
+  <v-stepper :items="['Local Tests of your Docker Submission', 'Add the Docker Submission', 'Final Checks']">
+  <template v-slot:item.1 style="min-height: 100vh">
+    <v-card title="Local Tests of your Docker Submission" flat>
+      <v-card-text>
+            <p>This form will guide you through the process of adding a new Docker submission. Please test that your Docker submission works as expected on your machine with the commands below and click on "Next" as soon as everything looks fine.</p>
+            <p>Please use the <a href="https://www.tira.io/c/support"> Forum</a> or contact the organizers if you face problems.</p>
+          </v-card-text>
+          <v-card-text>
+            <h3 class="text-h6 font-weight-light mb-6">Step-by-Step Guide to test your Docker Submission</h3>
+            <p>Before you submit your Docker submission, please test it on your machine using the following three steps:</p>
+            
+            <div class="my-3"/>
+
+            <code-snippet title="Install the TIRA CLI on your machine" code="pip3 install tira" expand_message="(1) Install the TIRA CLI"/>
+
+            <div class="my-3"/>
+
+            <code-snippet title="Execute your Docker Submission on a Small Example Dataset" :code="tira_initial_run_example" expand_message="(2)  Execute your submission on a small example dataset"/>
+
+              <div class="my-3"/>
+
+            <code-snippet title="Verify the Evaluator outputs to to ensure your Docker submission produces valid outputs" code="# The command above evaluated the outputs of your software 
+# Please verify that the evaluation in the directory tira-evaluation indicates that the outputs of your software are valid.
+cat tira-evaluation/evaluation.prototext" expand_message="(3) Verify the evaluator outputs to ensure all outputs are valid"/>
+          </v-card-text>
+    </v-card>
+  </template>
+
+  <template v-slot:item.2 style="min-height: 100vh">
+    <v-card title="Add the Docker Submission" flat>
+      <div class="pa-4 text-center">
+
+<h3 class="text-h6 font-weight-light my-6" v-if="is_ir_task">
+  Please select previous stages
+</h3>
+<p v-if="is_ir_task" class="mb-4">You can select multiple previous stages. The outputs of the previous stages are mounted into the container that executes the software.</p>
+<v-form ref="form" v-model="valid">
+<v-autocomplete v-if="is_ir_task" label="Previous Stages"
+                :items="all_previous_stages"
+                v-model="selectedDockerSoftware"
+                item-value="docker_software_id"
+                item-title="display_name"
+                clearable
+                multiple
+                chips/>
+
+<div class="text-center">
+<h3 class="text-h6 font-weight-light my-6">
+  Please specify your docker software
+</h3>
+  <p>A software submission consists of a docker image and the command that is executed in the docker image. Please specify both below.</p>
+
+  <p class="my-4 d-flex align-start"> Choose either an existing docker image... </p>
+<v-autocomplete
+    label="Docker Image"
+    :items="docker_images"
+    v-model="selectedDockerImage"
+    item-value="image"
+    item-title="title"
+    :loading="loading"
+    :disabled="loading"
+    clearable
+    :rules="[v => !!(v && v.length) || 'Please select the docker image for the execution.']"/>
+  <v-btn
+          class="mr-4"
+          color="primary"
+          :loading="refreshingInProgress"
+          @click="refreshImages()"
+        >
+          refresh images
+  </v-btn>
+  <span><br v-if="$vuetify.display.mdAndDown">last refreshed: {{docker_images_last_refresh}}</span>
+</div>
+   <div class="text-center mb-4">
+    <v-dialog
+      v-model="dialog"
+      width="auto"
+    >
+      <template v-slot:activator="{ props }" class="d-flex flex-column align-center" >
+        <div>
+        <v-btn
+          class="d-flex mr-2 mb-4 mt-6 align-start"
+          color="primary"
+          variant="plain"
+          v-bind="props"
+        >
+          <span v-if="!$vuetify.display.mdAndDown">...or see instructions on how to add a new docker image to tira</span>
+          <span v-if="$vuetify.display.mdAndDown">...or add new image</span>
+        </v-btn>
+       </div>
+      </template>
+
+      <v-card>
+        <v-card-text v-if="loading"><loading :loading="loading"/></v-card-text>
+        <v-card-text v-html="docker_software_help" v-if="!loading" />
+        <v-card-actions>
+          <v-btn color="primary" block @click="dialog = false">Close Information</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+<v-text-field v-model="runCommand" clearable
+              label="Command: mySoftware -c $inputDataset -r $inputRun -o $outputDir"
+              hint="Available variables: $inputDataset, $inputRun, $outputDir, $dataServer, and $token."
+              :rules="[v => !!(v && v.length) || 'Please select the run command for the execution.']"/>
+</v-form>
+
+</div>
+    </v-card>
+  </template>
+
+  <template v-slot:item.3 style="min-height: 100vh">
+    <v-card title="Final Checks'" flat>
+      <div class="text-center">
+            <h3 class="text-h6 font-weight-light my-6">
+              Double check your local run and submit
+            </h3>
+          </div>
+          <code-snippet title="Check the Software Submission on a Small Example Dataset" :code="double_check_tira_run_command" expand_message="Please test the configuration of your your submission on a small example dataset"/>
+        
+    </v-card>
+  </template>
+</v-stepper>
+  <!-- <div class="d-flex justify-lg-space-between">
     <v-card class="mx-auto mt-12" style="width: 100%;">
       <v-card-title class="text-h6 font-weight-regular d-flex justify-space-between">
         <span class="mr-3">{{ currentTitle }}</span>
@@ -177,7 +300,8 @@ cat tira-evaluation/evaluation.prototext" expand_message="(3) Verify the evaluat
     </v-card>
 
   </div>
-</div>
+   -->
+
 </template>
 
 <script lang="ts">
