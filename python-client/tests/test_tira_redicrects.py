c546358ee11d178fd68561955f83c630c79e57e8
Maik Fröbe <maik.froebe@uni-jena.de>
2024-02-29 09:23:15 +0100

    Improve archival of runs to zenodo: use run-ids

--- python-client/tests/test_tira_redicrects.py
+++ python-client/tests/test_tira_redicrects.py
@@ -6,7 +6,11 @@ import requests
 from hashlib import md5
 
 def md5_of_first_kilobyte_of_http_resource(url):
+    if not url.startswith('https://files.webis.de'):
+        raise ValueError(f'URL {url} is not from webis.de')
     return md5(requests.get(url, headers={'Range': f'bytes=0-1024'}).content).hexdigest()
+
+RUN_IDS = {"ir-benchmarks": {"tira-ir-starter": {"Index (tira-ir-starter-pyterrier)": {"msmarco-passage-trec-dl-2019-judged-20230107-training": "2023-01-07-22-09-56", "msmarco-passage-trec-dl-2020-judged-20230107-training": "2023-01-07-22-09-56", "antique-test-20230107-training": "2023-01-07-13-40-04", "vaswani-20230107-training": "2023-01-07-19-01-50", "cranfield-20230107-training": "2023-01-07-13-39-11", "medline-2004-trec-genomics-2004-20230107-training": "2023-01-07-19-37-49", "medline-2017-trec-pm-2017-20230211-training": "2023-02-11-20-52-47", "cord19-fulltext-trec-covid-20230107-training": "2023-01-08-15-18-20", "nfcorpus-test-20230107-training": "2023-02-09-15-46-37", "argsme-touche-2020-task-1-20230209-training": "2023-02-09-17-50-22", "argsme-touche-2021-task-1-20230209-training": "2023-02-09-17-50-31", "medline-2017-trec-pm-2018-20230211-training": "2023-02-11-15-15-35", "medline-2004-trec-genomics-2005-20230107-training": "2023-02-09-22-14-32", "trec-tip-of-the-tongue-dev-20230607-training": "2023-11-10-23-23-59", "longeval-short-july-20230513-training": "2023-11-10-23-22-59", "longeval-heldout-20230513-training": "2023-11-10-23-21-55", "longeval-long-september-20230513-training": "2023-11-10-21-09-17", "longeval-train-20230513-training": "2023-11-11-06-49-15"}}}}
 
 class TestRedirects(unittest.TestCase):
 
@@ -20,7 +24,7 @@ class TestRedirects(unittest.TestCase):
             ret[software] = {}
             for dataset in datasets_to_approve:
                 task, team, system = software.split('/')
-                run_url = f'https://www.tira.io/task/{task}/user/{team}/dataset/{dataset}/download/{system}.zip'
+                run_url = f'https://www.tira.io/task/{task}/user/{team}/dataset/{dataset}/download/{RUN_IDS[task][team][system][dataset]}.zip'
                 ret[software][dataset] = {
                     'redirect_url': redirects(software, dataset),
                     'run_execution': tira.get_run_execution_or_none(software, dataset),
--- python-client/tira/tira_redirects.py
+++ python-client/tira/tira_redirects.py
@@ -10,6 +10,7 @@ STATIC_REDIRECTS = {
                     # better caching: dl2020 and 2019 used the same corpus
                     'url': 'https://files.webis.de/data-in-production/data-research/tira-zenodo-dump-preparation/pyterrier-indexes/2023-01-07-22-09-56.zip',
                     'run_id': '2023-01-07-22-09-56',
+
                     #'url': 'https://files.webis.de/data-in-production/data-research/tira-zenodo-dump-preparation/pyterrier-indexes/2023-02-09-19-37-45.zip',
                     #'run_id': '2023-02-09-19-37-45',
                 },
@@ -86,6 +87,14 @@ STATIC_REDIRECTS = {
     }
 }
 
+RUN_ID_TO_SYSTEM = {}
+for task in STATIC_REDIRECTS.keys():
+    for team in STATIC_REDIRECTS[task].keys():
+        for system_name in STATIC_REDIRECTS[task][team].keys():
+            system = STATIC_REDIRECTS[task][team][system_name]
+            for config in system.values():
+                RUN_ID_TO_SYSTEM[config['run_id']] = system_name
+
 #{"ir-benchmarks": {"tira-ir-starter": {"Index (tira-ir-starter-pyterrier)": {"msmarco-passage-trec-dl-2019-judged-20230107-training": "2023-01-07-22-09-56", "msmarco-passage-trec-dl-2020-judged-20230107-training": "2023-02-09-19-37-45", "antique-test-20230107-training": "2023-01-07-13-40-04", "vaswani-20230107-training": "2023-01-07-19-01-50", "cranfield-20230107-training": "2023-01-07-13-39-11", "medline-2004-trec-genomics-2004-20230107-training": "2023-01-07-19-37-49", "medline-2017-trec-pm-2017-20230211-training": "2023-02-11-20-52-47", "cord19-fulltext-trec-covid-20230107-training": "2023-01-08-15-18-20", "nfcorpus-test-20230107-training": "2023-02-09-15-46-37", "argsme-touche-2020-task-1-20230209-training": "2023-02-09-17-50-22", "argsme-touche-2021-task-1-20230209-training": "2023-02-09-17-50-31", "medline-2017-trec-pm-2018-20230211-training": "2023-02-11-15-15-35", "medline-2004-trec-genomics-2005-20230107-training": "2023-02-09-22-14-32", "trec-tip-of-the-tongue-dev-20230607-training": "2023-11-10-23-23-59", "longeval-short-july-20230513-training": "2023-11-10-23-22-59", "longeval-heldout-20230513-training": "2023-11-10-23-21-55", "longeval-long-september-20230513-training": "2023-11-10-21-09-17", "longeval-train-20230513-training": "2023-11-11-06-49-15"}}}}
 
 
@@ -96,7 +105,8 @@ def redirects(approach=None, dataset=None, url=None):
             #/task/{task}/user/{team}/dataset/{dataset}/download/{run_id}.zip
             ret = url.split('/task/')[1]
             ret = ret.split('/')
-            task, team, dataset, system = ret[0], ret[2], ret[4], ret[6].replace('.zip', '')
+            task, team, dataset, run_id = ret[0], ret[2], ret[4], ret[6].replace('.zip', '')
+            system = RUN_ID_TO_SYSTEM.get(run_id, None)
         else:
             return default_ret
 
