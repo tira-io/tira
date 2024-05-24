df707bf8cb636bb8f37c628feb424b80f392e49e
Tim <timhagenmail@googlemail.com>
2024-05-24 13:36:29 +0200

    use tira-logo.svg for the logo

--- documentation/conf.py
+++ documentation/conf.py
@@ -65,7 +65,7 @@ html_theme = 'furo'
 html_title = 'TIRA'
 language = 'en'
 
-html_logo = "https://github.com/tira-io/tira-branding/raw/master/tira-icons/logo-tira-120x120-transparent.png"
+html_logo = "https://assets.tira.io/tira-icons/tira-logo.svg"
 html_theme_options: Dict[str, Any] = {
     "footer_icons": [
         {
@@ -94,8 +94,8 @@ umlet_binary_path = "/usr/share/umlet/Umlet/umlet.sh"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
-# html_css_files = [
-#     'css/custom.css',
-# ]
+html_css_files = [
+    'custom.css',
+]
