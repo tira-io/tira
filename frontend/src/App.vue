7e928d8c6b1fb54bb20469b8fa259d34ebd190b1
Maik Fröbe <maik.froebe@uni-jena.de>
2024-10-02 15:45:41 +0200

    prepare App meni

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,50 @@
 <template>
   <v-app>
+    <v-app-bar style="background-color: #323232; color: white;">
+
+      <v-container style="max-width: 1110px;" class="d-md-none">
+        <v-row>
+          <v-col>
+            <a href="/">
+              <img id="site-logo" src="https://assets.tira.io/tira-icons/tira-banner-120x360-dark.png" alt="TIRA" style="height: 2.667em">
+            </a>
+          </v-col>
+
+          <v-col class="text-right">
+            <v-btn icon>
+              <v-icon>mdi-menu</v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+
+      </v-container>
+      
+      <v-container style="max-width: 1110px;" class="d-none d-md-block">
+        <v-row>
+          <v-col>
+        <a href="/">
+          <img id="site-logo" src="https://assets.tira.io/tira-icons/tira-banner-120x360-dark.png" alt="TIRA" style="height: 2.667em">
+        </a>
+      </v-col>
+
+        <v-col class="text-right">
+          <v-btn>
+            API
+          </v-btn>
+          <v-btn>
+            Forum
+          </v-btn>
+          <v-btn icon>
+            <v-icon>mdi-menu</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <img width="48" height="48" src="https://api.tira.io/user_avatar/api.tira.io/maik_froebe/96/5_2.png" style="border-radius: 50%;">
+          </v-btn>
+        </v-col>
+      </v-row>
+      </v-container>
+    </v-app-bar>
     <v-main class="position-relative">
       <tira-alert class="position-absolute"/>
       <router-view></router-view>
