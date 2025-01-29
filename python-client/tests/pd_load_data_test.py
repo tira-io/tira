a8df87d949de12716da690197b027b76e1b12441
Maik Fröbe <maik.froebe@uni-jena.de>
2025-01-29 07:47:08 +0100

    mf

--- python-client/tests/pd_load_data_test.py
+++ python-client/tests/pd_load_data_test.py
@@ -53,3 +53,37 @@ class TestLocalExecutionIntegrationTest(unittest.TestCase):
         self.assertTrue("query_id" not in last_line)
         self.assertTrue("unknown_doc_id" not in last_line)
         self.assertTrue("qrel_unknown_doc" not in last_line)
+
+    def test_pd_load_truths_local_cached_zip_single_identifier(self):
+        tira = Client(tira_cache_dir="tests/resources/local_cached_zip")
+        actual = tira.pd.truths("task-does-not-exist/dataset-does-not-exist-20241201-training")
+        self.assertEqual(13, len(actual))
+        first_line = actual.iloc[0].to_dict()
+        last_line = actual.iloc[12].to_dict()
+
+        self.assertEqual("5a8865b0-19d7-4b33-bbe3-2f64ad54557f", first_line["id"])
+        self.assertEqual("1051399", first_line["query_id"])
+        self.assertEqual(3376628, first_line["unknown_doc_id"])
+        self.assertEqual(0, first_line["qrel_unknown_doc"])
+
+        self.assertEqual("449f69fa-df0e-4c9e-aead-61983ce9eaa8", last_line["id"])
+        self.assertEqual("833860", last_line["query_id"])
+        self.assertEqual(2830558, last_line["unknown_doc_id"])
+        self.assertEqual(0, last_line["qrel_unknown_doc"])
+
+    def test_pd_load_inputs_local_cached_zip_single_identifier(self):
+        tira = Client(tira_cache_dir="tests/resources/local_cached_zip")
+        actual = tira.pd.inputs("task-does-not-exist/dataset-does-not-exist-20241201-training")
+        self.assertEqual(13, len(actual))
+        first_line = actual.iloc[0].to_dict()
+        last_line = actual.iloc[12].to_dict()
+
+        self.assertEqual("5a8865b0-19d7-4b33-bbe3-2f64ad54557f", first_line["id"])
+        self.assertTrue("query_id" not in first_line)
+        self.assertTrue("unknown_doc_id" not in first_line)
+        self.assertTrue("qrel_unknown_doc" not in first_line)
+
+        self.assertEqual("449f69fa-df0e-4c9e-aead-61983ce9eaa8", last_line["id"])
+        self.assertTrue("query_id" not in last_line)
+        self.assertTrue("unknown_doc_id" not in last_line)
+        self.assertTrue("qrel_unknown_doc" not in last_line)
--- python-client/tira/pandas_integration.py
+++ python-client/tira/pandas_integration.py
@@ -43,7 +43,6 @@ class PandasIntegration:
             pd.DataFrame: The run file parsed to a pandas DataFrame.
         """
         import pandas as pd
-
         from tira.ir_datasets_util import translate_irds_id_to_tirex
 
         task, team, software = approach.split("/")
@@ -95,6 +94,12 @@ class PandasIntegration:
                 ret.add(i)
 
         return sorted(list(ret))
+
+    def __extract_task_and_dataset_id(self, task, dataset):
+        if dataset == None:
+            task, dataset = task.split("/")
+
+        return task, dataset
 
     def transform_queries(
         self, approach: str, dataset: str, file_selection: Tuple[str, ...] = ("/*.jsonl", "/*.jsonl.gz")
@@ -180,7 +185,7 @@ class PandasIntegration:
 
         return ret
 
-    def inputs(self, task, dataset, file_selection=("/*.jsonl", "/*.jsonl.gz"), dtype=PANDAS_DTYPES):
+    def inputs(self, task, dataset=None, file_selection=("/*.jsonl", "/*.jsonl.gz"), dtype=PANDAS_DTYPES):
         """Load the inputs to systems for a task from tira.
 
         Args:
@@ -199,6 +204,7 @@ class PandasIntegration:
         """
         import pandas as pd
 
+        task, dataset = self.__extract_task_and_dataset_id(task, dataset)
         matching_files = self.__matching_dataset_files(task, dataset, False, file_selection)
 
         if len(matching_files) == 0:
@@ -210,7 +216,7 @@ class PandasIntegration:
 
         return pd.read_json(matching_files[0], lines=True, dtype=dtype)
 
-    def truths(self, task, dataset, file_selection=("/*.jsonl", "/*.jsonl.gz"), dtype=PANDAS_DTYPES):
+    def truths(self, task, dataset=None, file_selection=("/*.jsonl", "/*.jsonl.gz"), dtype=PANDAS_DTYPES):
         """Load the truths, i.e., ground truth labels, for a task from tira.
 
         Args:
@@ -229,6 +235,7 @@ class PandasIntegration:
         """
         import pandas as pd
 
+        task, dataset = self.__extract_task_and_dataset_id(task, dataset)
         matching_files = self.__matching_dataset_files(task, dataset, True, file_selection)
 
         if len(matching_files) == 0:
