ed544cd606b7673aa0a0de86617d127d5ce1d463
Tim Hagen <timhagenmail@googlemail.com>
2025-08-20 11:28:55 +0200

    updated devcontainer configuration and load configuration in worker

--- worker/src/tira_worker/_tasks.py
+++ worker/src/tira_worker/_tasks.py
@@ -1,16 +1,26 @@
 from celery import Celery
+from tira.rest_api_client import Client as RestClient
+from tira.rest_api_client import TiraClient
 
-# TODO: make configurable using tira-application-config.yml
-backend_url = "rpc://localhost"
-broker_url = "amqp://localhost"
+from .settings import QUEUE_BROKER_URL, QUEUE_RESULTS_BACKEND_URL, TIRA_API_KEY
 
-app = Celery("tira-tasks", backend=backend_url, broker=broker_url)
+app = Celery("tira-tasks", backend=QUEUE_RESULTS_BACKEND_URL, broker=QUEUE_BROKER_URL)
 
 
 @app.task
-def evaluate(text: str):
-    # TODO: Implement me
-    from time import sleep
-
-    sleep(2)
-    return f"Hello {text}"
+def evaluate(runid: str) -> None:
+    # TODO: init RestClient from configuration
+    client: "TiraClient" = RestClient(base_url=None, api_key=TIRA_API_KEY, base_url_api="")
+    # TODO: load values using REST-API / Client from runid.
+    raise NotImplementedError
+    client.submit_code(
+        path=None,  # TODO
+        task_id=None,  # TODO
+        command=None,  # TODO
+        dataset_id=None,  # TODO
+        user_id=None,  # TODO
+        docker_file=None,  # TODO
+        dry_run=False,
+        allow_network=False,
+        mount_hf_model=None,  # TODO
+    )
