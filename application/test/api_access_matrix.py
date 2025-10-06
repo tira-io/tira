376586924da9c0630add1d593fe920e1acc8c355
Maik Fröbe <maik.froebe@uni-jena.de>
2025-10-06 21:55:08 +0200

    mf

--- application/test/api_access_matrix.py
+++ application/test/api_access_matrix.py
@@ -2206,6 +2206,19 @@ API_ACCESS_MATRIX = [
             ADMIN: 404,
         },
     ),
+    route_to_test(
+        url_pattern="v1/admin/upload-response/<str:dataset_id>/<str:vm_id>",
+        params={"dataset_id": "does-not-exist", "vm_id": "does-not-exist"},
+        params={},
+        method="GET",
+        group_to_expected_status_code={
+            GUEST: 403,
+            PARTICIPANT: 403,
+            ORGANIZER_WRONG_TASK: 403,
+            ORGANIZER: 200,
+            ADMIN: 200,
+        },
+    ),
     # The following v1/ endpoints should be restricted to only allow admin-access for now
     route_to_test(
         url_pattern="v1/evaluations/",
