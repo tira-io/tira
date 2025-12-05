6717fb3910d69b25cce059f8df6e122a2d8f7415
Maik Fröbe <maik.froebe@uni-jena.de>
2025-12-05 09:44:40 +0100

    update tirex-tracker

--- python-client/tira/tirex_tracker.py
+++ python-client/tira/tirex_tracker.py
@@ -8,9 +8,9 @@ def tirex_tracker_mounts_or_none():
 
 
 def find_tirex_tracker_executable_or_none():
-    # wget https://github.com/tira-io/tirex-tracker/releases/download/0.2.5/measure-0.2.5-linux -O 'tirex-tracker-0.2.5-linux'
-    # chmod +x tirex-tracker-0.2.5-linux
-    ret = Path(__file__).parent / "tirex-tracker-0.2.5-linux"
+    # wget https://github.com/tira-io/tirex-tracker/releases/download/0.2.18/measure-0.2.18-linux -O 'tirex-tracker-0.2.18-linux'
+    # chmod +x tirex-tracker-0.2.18-linux
+    ret = Path(__file__).parent / "tirex-tracker-0.2.18-linux"
     ret = ret.resolve().absolute()
 
     return ret if ret.exists() else None
