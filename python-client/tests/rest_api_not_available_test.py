40ae07da8f703be78121aa24de026e0d87061a08
Maik Fröbe <maik.froebe@uni-jena.de>
2026-03-06 18:48:42 +0100

    allow to download things

--- python-client/tests/rest_api_not_available_test.py
+++ python-client/tests/rest_api_not_available_test.py
@@ -1,38 +1,38 @@
 import shutil
 import unittest
 from pathlib import Path
-from tempfile import TemporaryDirectory
 
 from tira.rest_api_client import Client
+from tira.third_party_integrations import temporary_directory
 
 
 class TestRestApiNotAvailableTest(unittest.TestCase):
 
     def clean_tira_cache_dir(self):
-        with TemporaryDirectory() as tmp_dir:
-            src_dir = Path(__file__).parent / "resources" / "tira-cache-without-production-system"
-            shutil.copy(src_dir / ".tira-settings.json", tmp_dir)
+        tmp_dir = temporary_directory()
+        src_dir = Path(__file__).parent / "resources" / "tira-cache-without-production-system"
+        shutil.copy(src_dir / ".tira-settings.json", tmp_dir)
 
-            return tmp_dir
+        return tmp_dir
 
     def test_client_can_be_created(self):
-        with self.clean_tira_cache_dir() as cache_dir:
-            tira = Client(tira_cache_dir=Path(cache_dir))
-            self.assertIsNotNone(tira)
+        cache_dir = self.clean_tira_cache_dir()
+        tira = Client(tira_cache_dir=Path(cache_dir))
+        self.assertIsNotNone(tira)
 
     def test_all_datasets_works_for_tirex(self):
-        with self.clean_tira_cache_dir() as cache_dir:
-            tira = Client(tira_cache_dir=Path(cache_dir))
-            actual = tira.datasets("ir-benchmarks")
-            self.assertIn("antique-test-20230107-training", actual)
+        cache_dir = self.clean_tira_cache_dir()
+        tira = Client(tira_cache_dir=Path(cache_dir))
+        actual = tira.datasets("ir-benchmarks")
+        self.assertIn("antique-test-20230107-training", actual)
 
     def test_public_system_details(self):
-        with self.clean_tira_cache_dir() as cache_dir:
-            tira = Client(tira_cache_dir=Path(cache_dir))
-            actual = tira.public_system_details("tira-ir-starter", "BM25 Re-Rank (tira-ir-starter-pyterrier)")
-            self.assertIn("public_image_name", actual)
+        cache_dir = self.clean_tira_cache_dir()
+        tira = Client(tira_cache_dir=Path(cache_dir))
+        actual = tira.public_system_details("tira-ir-starter", "BM25 Re-Rank (tira-ir-starter-pyterrier)")
+        self.assertIn("public_image_name", actual)
 
-            self.assertEqual(
-                "docker.io/webis/ir-benchmarks-submissions:tira-ir-starter-pyterrier-0-0-1-tira-docker-software-id-plastic-cabin",
-                actual["public_image_name"],
-            )
+        self.assertEqual(
+            "docker.io/webis/ir-benchmarks-submissions:tira-ir-starter-pyterrier-0-0-1-tira-docker-software-id-plastic-cabin",
+            actual["public_image_name"],
+        )
