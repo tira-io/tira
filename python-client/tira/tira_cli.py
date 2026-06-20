425aab101f82f43f4fb7cb0c86e238279c4a3e29
Maik Fröbe <maik.froebe@uni-jena.de>
2026-06-20 12:11:14 +0200

    mf

--- python-client/tira/tira_cli.py
+++ python-client/tira/tira_cli.py
@@ -328,7 +328,6 @@ def run_local(input: str, approach: str, cpus: Optional[int], memory: Optional[s
         cpu_count=cpus,
         mem_limit=memory,
         mount_directory=None,
-        platform="linux/amd64",
     ))
 
     details = {"system": system_details, "input": input}
